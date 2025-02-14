b75d6ed624e21f017632814d26adda8bc7dd6ab6
Phillip Weinberg <weinbe58@gmail.com>
2025-02-14 11:39:16 -0500

    More binding doc strings.  (#109)

--- src/bloqade/qasm2/glob.py
+++ src/bloqade/qasm2/glob.py
@@ -12,4 +12,13 @@ from .dialects import glob
 @wraps(glob.UGate)
 def u(
     theta: float, phi: float, lam: float, registers: ilist.IList[QReg, Any] | list
-) -> None: ...
+) -> None:
+    """Apply a U gate to all qubits in the input registers.
+
+    Args:
+        theta (float): The angle theta.
+        phi (float): The angle phi.
+        lam (float): The angle lam.
+        registers (IList[QReg] | list[QReg]): The registers to apply the gate to.
+
+    """
--- src/bloqade/qasm2/parallel.py
+++ src/bloqade/qasm2/parallel.py
@@ -12,14 +12,37 @@ from .dialects import parallel
 @wraps(parallel.CZ)
 def cz(
     ctrls: ilist.IList[Qubit, Any] | list, qargs: ilist.IList[Qubit, Any] | list
-) -> None: ...
+) -> None:
+    """Apply a controlled-Z gate to input qubits in parallel.
+
+    Args:
+        ctrls (IList[Qubit] | list[Qubit]): The control qubits.
+        qargs (IList[Qubit] | list[Qubit]): The target qubits.
+
+    """
 
 
 @wraps(parallel.UGate)
 def u(
     qargs: ilist.IList[Qubit, Any] | list, theta: float, phi: float, lam: float
-) -> None: ...
+) -> None:
+    """Apply a U gate to input qubits in parallel.
+
+    Args:
+        qargs (IList[Qubit] | list[Qubit]): The target qubits.
+        theta (float): The angle theta.
+        phi (float): The angle phi.
+        lam (float): The angle lam.
+
+    """
 
 
 @wraps(parallel.RZ)
-def rz(qargs: ilist.IList[Qubit, Any] | list, theta: float) -> None: ...
+def rz(qargs: ilist.IList[Qubit, Any] | list, theta: float) -> None:
+    """Apply a RZ gate to input qubits in parallel.
+
+    Args:
+        qargs (IList[Qubit] | list[Qubit]): The target qubits.
+        theta (float): The angle theta.
+
+    """
