1bc704492ed081c13609550ea56ba0a83d955a87
John Long <johnzl@protonmail.ch>
2025-02-14 12:57:57 -0500

    Improve Log GHZ example (#101)
    Polished some of the text on the log-depth GHZ example.
    
    I want to clarify, is the number of CXs technically N - 1? where N is
    2^n qubits? That being said, probably worth @kaihsin taking a final look
    before approving this (:
    
    Co-authored-by: Phillip Weinberg <weinbe58@gmail.com>

--- docs/digital/examples/ghz.py
+++ docs/digital/examples/ghz.py
@@ -1,6 +1,12 @@
 # %% [markdown]
-# # GHZ State Preparation with Parallelism.
-# In this example, we will consider the GHZ state preparation circuit with 2^n qubits
+# # GHZ State Preparation with Parallelism
+# In this example, we will implement a *Greenberger-Horne-Zeilinger* (GHZ) state preparation circuit with $N = 2^n$ qubits.
+#
+# First, we will present the standard linear-depth construction in Bloqade but later we will present a log-depth
+# construction that achieves the same result. We then take this one step further and use the fact that Bloqade
+# (and QuEra's neutral atom hardware!) support *parallel* gates, allowing for the application of the same gate
+# across multiple qubits simultaneously. Combined with the fact that atom *shuttling* allows for arbitrary
+# connectivity, we can also decrease the circuit execution depth from $N + 1$ to just $n$.
 
 # %%
 import math
@@ -9,9 +15,10 @@ from bloqade import qasm2
 from kirin.dialects import ilist
 
 # %% [markdown]
-# ## Simple linear depth impl of ghz state prep
-# A simple GHZ state preparation circuit can be done with N CX gates and 1 H gate.
-# This gives circuit execution depth of N+1.
+# ## Simple Linear Depth Implementation of a GHZ State Preparation Circuit
+#
+# A simple GHZ state preparation circuit can be built with $N$ CX gates and $1$ H gate.
+# This gives the circuit an execution depth of $N+1$.
 
 
 # %%
@@ -22,7 +29,11 @@ def ghz_linear(n: int):
     def ghz_linear_program():
 
         qreg = qasm2.qreg(n_qubits)
+        # Apply a Hadamard on the first qubit
         qasm2.h(qreg[0])
+        # Create a cascading sequence of CX gates
+        # necessary for quantum computers that
+        # only have nearest-neighbor connectivity between qubits
         for i in range(1, n_qubits):
             qasm2.cx(qreg[i - 1], qreg[i])
 
@@ -30,14 +41,14 @@ def ghz_linear(n: int):
 
 
 # %% [markdown]
-# ## log depth impl of ghz state prep
-# Let's take a look how we can rewrite the circuit toward more QuEra's hardware friendly circuit.
-# We can rewrite the GHZ state preparation circuit with log(N) depth by rearranging the CX gates
-# [citation](https://arxiv.org/abs/2101.08946 – Mooney, White, Hill, Hollenberg)
+# ## Log-depth Implementation of a GHZ State Preparation Circuit
 #
-# Note it is important to separate the concept of circuit depth and circuit execution depth.
-# For example, in the following implementation, each CX gate instruction inside the for loop are executed in sequence.
-# So even thought the circuit depth is N/2 + 1. The circuit execution depth is still N + 1.
+# Let's take a look how we can rewrite the circuit to take advantage of QuEra's hardware capabilities.
+# We can achieve log(N) circuit depth by [rearranging the CX gates (see *Mooney, White, Hill, Hollenberg* - 2021)](https://arxiv.org/abs/2101.08946).
+#
+# Before going any further, it's worth distinguishing between the concept of **circuit depth** and **circuit execution depth**.
+# For example, in the following implementation, each CX gate instruction inside the for-loop is executed in sequence.
+# So even thought the circuit depth is $N/2 + 1$, the circuit execution depth is still $N + 1$.
 
 
 # %%
@@ -45,7 +56,7 @@ def ghz_log_depth(n: int):
     n_qubits = int(2**n)
 
     @qasm2.extended
-    def layer(i_layer: int, qreg: qasm2.QReg):
+    def layer_of_cx(i_layer: int, qreg: qasm2.QReg):
         step = n_qubits // (2**i_layer)
         for j in range(0, n_qubits, step):
             qasm2.cx(ctrl=qreg[j], qarg=qreg[j + step // 2])
@@ -57,23 +68,24 @@ def ghz_log_depth(n: int):
 
         qasm2.h(qreg[0])
         for i in range(n):
-            layer(i_layer=i, qreg=qreg)
+            layer_of_cx(i_layer=i, qreg=qreg)
 
     return ghz_log_depth_program
 
 
 # %% [markdown]
-# ## Native gate set and parallelism
-# On our digital quantum computer, by nature can execute native gate in parallel in an single instruction/ execution cycle.
+# ## Our Native Gate Set and Parallelism
+# By nature, our digital quantum computer can execute native gates in parallel in an single instruction/ execution cycle.
 # The concept is very similar to the SIMD (Single Instruction, Multiple Data) in classical computing.
-# On our hardware, there are two important factor to be consider:
-# 1. the native gate set is arbitrary (parallel) rotations and (parallel) CZ gates.
-# 2. Our atom shuttling architecture allows arbitrary qubit connectivity. This means that our parallel instruction is not limited to certain hardware connectivity (for example nearest neighbor connectivity).
 #
-# Let's try to rewrite the `layer` subroutinme.
-# We know that the CX gate can be decomposed into CZ gate with two single qubit gates Ry(-pi/2) and Ry(pi/2) acting on the target qubits.
-# After such decomposition, we can now using our parallel gate instructions `parallel.u` and `parallel.cz`.
-# With the following modification, we can further reduce the circuit execution depth to n (log of total qubit number N)
+# On our hardware, there are two important factors to be considered:
+# 1. the native gate set allows for arbitrary (parallel) rotations and (parallel) CZ gates.
+# 2. Our atom shuttling architecture allows arbitrary qubit connectivity. This means that our parallel instruction is not limited to fixed connectivity (for example nearest neighbor connectivity).
+#
+# With this in mind, we can rewrite the `layer` subroutine to now use the `qasm2.parallel` dialect in Bloqade.
+# We know that the CX gate can be decomposed into a CZ gate with two single-qubit gates $R_y(-\pi/2)$ and $R_y(\pi/2)$ acting on the target qubits.
+# With this decomposition in mind, we can now using our parallel gate instructions `parallel.u` and `parallel.cz`.
+# With the following modification, we can further reduce the circuit execution depth to just $n$ (log of the total number of qubits $N$)
 
 
 # %%
@@ -114,8 +126,8 @@ def ghz_log_simd(n: int):
 
 
 # %% [markdown]
-# Note on using closure to capture global variable:
-# Since qasm2 does not allow main program with arguments, so we need to put the program in a closure.
-# our kirin compiler toolchain can capture the global variable inside the closure.
-# In this case, the n_qubits will be captured upon calling the `ghz_half_simd(n_qubits)` python function,
-# As a result, the return qasm2 program will not have any arguments.
+# A Note on using closures to capture global variables:
+# Since Bloqade's `qasm2` does not allow a main program with arguments, we need to put the program in a closure.
+# Our Kirin compiler toolchain can capture the global variable inside the closure.
+# In this case, the `n_qubits` will be captured upon calling the `ghz_log_simd(n_qubits)` python function.
+# As a result, the returned `qasm2` program will not have any arguments.
