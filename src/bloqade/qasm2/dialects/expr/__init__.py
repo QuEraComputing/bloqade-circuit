19da2743a78c3acd1846c809a0d1eea512b8df38
Roger-luo <rogerluo.rl18@gmail.com>
2025-04-12 12:06:05 -0400

    mark some files private

--- src/bloqade/qasm2/dialects/expr/__init__.py
+++ src/bloqade/qasm2/dialects/expr/__init__.py
@@ -1,3 +1,3 @@
-from . import emit as emit, interp as interp, from_python as from_python
+from . import _emit as _emit, _interp as _interp, _from_python as _from_python
 from .stmts import *  # noqa: F403
 from ._dialect import dialect as dialect
