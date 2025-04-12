a76d54d9a0345703fc5f2d3d8be04881b542899e
Phillip Weinberg <weinbe58@gmail.com>
2025-04-12 14:14:50 -0400

    removing unneeded pass (#144)

--- src/bloqade/squin/groups.py
+++ src/bloqade/squin/groups.py
@@ -2,8 +2,6 @@ from kirin import ir, passes
 from kirin.prelude import structural_no_opt
 from kirin.dialects import ilist
 
-from bloqade.qasm2.rewrite.desugar import IndexingDesugarPass
-
 from . import op, wire, qubit
 
 
@@ -12,7 +10,6 @@ def kernel(self):
     fold_pass = passes.Fold(self)
     typeinfer_pass = passes.TypeInfer(self)
     ilist_desugar_pass = ilist.IListDesugar(self)
-    indexing_desugar_pass = IndexingDesugarPass(self)
 
     def run_pass(method, *, fold=True, typeinfer=True):
         method.verify()
@@ -22,7 +19,6 @@ def kernel(self):
         if typeinfer:
             typeinfer_pass(method)
         ilist_desugar_pass(method)
-        indexing_desugar_pass(method)
         if typeinfer:
             typeinfer_pass(method)  # fix types after desugaring
             method.code.typecheck()
