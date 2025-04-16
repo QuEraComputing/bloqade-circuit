551723e48e765861517c0f9513c1bbb83d58b0c4
Phillip Weinberg <weinbe58@gmail.com>
2025-04-16 10:21:54 -0400

    adding test

--- test/qasm2/test_lowering.py
+++ test/qasm2/test_lowering.py
@@ -1,5 +1,7 @@
 import textwrap
 
+from kirin.dialects import func
+
 from bloqade import qasm2
 from bloqade.qasm2.parse.lowering import QASM2
 
@@ -17,6 +19,17 @@ CX q[0], q[1];
 rx(pi/2) q[0];
 """
 )
-ast = qasm2.parse.loads(lines)
-code = QASM2(qasm2.main).run(ast)
-code.print()
+
+
+def test_run_lowering():
+    ast = qasm2.parse.loads(lines)
+    code = QASM2(qasm2.main).run(ast)
+    code.print()
+
+
+def test_loads():
+
+    kernel = QASM2(qasm2.main).loads(lines, "test", returns="c")
+    qasm2.main.run_pass(kernel)  # type: ignore
+    assert isinstance((ret := kernel.callable_region.blocks[0].last_stmt), func.Return)
+    assert ret.value.type.is_equal(qasm2.types.CRegType)
