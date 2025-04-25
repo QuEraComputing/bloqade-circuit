a2ab3a5fde08d8fdd9c874bc6689a893f31f7415
John Long <johnzl@protonmail.ch>
2025-04-25 17:16:19 -0400

    initial noise statements sketch

---
+++ src/bloqade/squin/noise/__init__.py
@@ -0,0 +1,31 @@
+# Put all the proper wrappers here
+
+# from kirin.types import Float, Tuple, Vararg
+from kirin.lowering import wraps as _wraps
+
+from bloqade.squin.op.types import Op
+
+from . import stmts as stmts
+
+
+@_wraps(stmts.PauliError)
+def pauli_error(basis: Op, p: float) -> Op: ...
+
+
+@_wraps(stmts.PPError)
+def pp_error(op: Op, p: float) -> Op: ...
+
+
+@_wraps(stmts.Depolarize)
+def depolarize(n_qubits: int, p: float) -> Op: ...
+
+
+# How do you give `types.Tuple[types.Vararg(types.Float)])` nicely to
+# the wrapper?
+
+# @_wraps(stmts.PauliChannel)
+# def pauli_channel(n_qubits: int, params:) -> Op:...
+
+
+@_wraps(stmts.QubitLoss)
+def qubit_loss(p: float) -> Op: ...
