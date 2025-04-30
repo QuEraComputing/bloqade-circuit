adadc4d29f4e0e9ef535822de38dde6651b2b95e
David Plankensteiner <da.plankensteiner@gmail.com>
2025-04-30 16:50:04 +0200

    Simplify for_loop impl

--- src/bloqade/analysis/fidelity/impls.py
+++ src/bloqade/analysis/fidelity/impls.py
@@ -67,34 +67,11 @@ class ScfFidelityMethodTable(interp.MethodTable):
     def for_loop(
         self, interp: FidelityAnalysis, frame: interp.Frame[EmptyLattice], stmt: For
     ):
-
         if not isinstance(hint := stmt.iterable.hints.get("const"), const.Value):
             # NOTE: not clear how long this loop is
             # TODO: should we at least count the body once?
             return
 
-        current_gate_fidelity = interp._current_gate_fidelity
-        current_atom_survival = interp._current_atom_survival_probability
-
-        # NOTE: we reset the interpreter fidelity and evaluate the fidelity for the body only once
-        interp._current_gate_fidelity = 1.0
-        interp._current_atom_survival_probability = 1.0
-        for s in stmt.body.stmts():
-            stmt_impl = interp.lookup_registry(frame=frame, stmt=s)
-            if stmt_impl is None:
-                continue
-
-            stmt_impl(interp=interp, frame=frame, stmt=s)
-
-        loop_body_gate_fidelity = interp._current_gate_fidelity
-        loop_body_atom_survival = interp._current_atom_survival_probability
-
-        # NOTE: reset current fidelity now in case of 0 iterations
-        interp._current_gate_fidelity = current_gate_fidelity
-        interp._current_atom_survival_probability = current_atom_survival
-
-        # NOTE: now we simply decrease the fidelity according to the number of iterations
-        iterable = hint.data
-        for _ in iterable:
-            interp._current_gate_fidelity *= loop_body_gate_fidelity
-            interp._current_atom_survival_probability *= loop_body_atom_survival
+        for _ in hint.data:
+            for s in stmt.body.stmts():
+                interp.eval_stmt(frame=frame, stmt=s)
