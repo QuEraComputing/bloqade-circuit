c4560b15584b0d8f1f11de24b87d870d4d23375a
David Plankensteiner <da.plankensteiner@gmail.com>
2025-04-30 16:16:38 +0200

    Fix renaming to NumberType

--- src/bloqade/squin/op/stmts.py
+++ src/bloqade/squin/op/stmts.py
@@ -2,7 +2,7 @@ from kirin import ir, types, lowering
 from kirin.decl import info, statement
 
 from .types import OpType
-from .number import Number
+from .number import NumberType
 from .traits import Unitary, HasSites, FixedSites, MaybeUnitary
 from ._dialect import dialect
 
@@ -54,7 +54,7 @@ class Scale(CompositeOp):
     traits = frozenset({ir.Pure(), lowering.FromPythonCall(), MaybeUnitary()})
     is_unitary: bool = info.attribute(default=False)
     op: ir.SSAValue = info.argument(OpType)
-    factor: ir.SSAValue = info.argument(Number)
+    factor: ir.SSAValue = info.argument(NumberType)
     result: ir.ResultValue = info.result(OpType)
 
 
