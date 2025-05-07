97901febe608cc7a45200523a05908b1079a4b10
David Plankensteiner <da.plankensteiner@gmail.com>
2025-05-07 14:21:11 +0200

    Restrict qasm2 parser so it rejects multiple expressions in if body

--- test/qasm2/parse/test_roundtrip.py
+++ test/qasm2/parse/test_roundtrip.py
@@ -1,30 +1,28 @@
 import os
 import pathlib
 
+import pytest
+from lark.exceptions import UnexpectedToken
+
 from bloqade.qasm2.parse import loads, spprint, loadfile
-from bloqade.qasm2.parse.parser import qasm2_parser as lark_parser
 
 
-def roundtrip(file):
-    ast1 = loadfile(os.path.join(os.path.dirname(__file__), "programs", file))
+def roundtrip(file, dirname):
+    ast1 = loadfile(os.path.join(os.path.dirname(__file__), dirname, file))
     ast2 = loads(spprint(ast1))
     return ast1 == ast2
 
 
 def test_roundtrip():
-    path = pathlib.Path(__file__).parent / "programs"
+    dirname = "programs"
+    path = pathlib.Path(__file__).parent / dirname
     for file in path.glob("*.qasm"):
-        assert roundtrip(file.name), f"Failed roundtrip for {file}"
+        assert roundtrip(file.name, dirname), f"Failed roundtrip for {file}"
 
 
-if __name__ == "__main__":
-    filepath = os.path.join(os.path.dirname(__file__), "programs", "global.qasm")
-    print(filepath)
-    with open(filepath) as f:
-        qasm_str = f.read()
-
-    parse_tree = lark_parser.parse(qasm_str)  # raw parsing seems to be fine
-    print(parse_tree.pretty())
-
-    ast = loads(qasm_str)
-    print(spprint(ast))
+def test_invalids():
+    dirname = "invalid_programs"
+    path = pathlib.Path(__file__).parent / dirname
+    for file in path.glob("*.qasm"):
+        with pytest.raises(UnexpectedToken):
+            roundtrip(file.name, dirname)
