81f96f7de526e00c7d4036ab3cd54f11c25641ab
John Long <johnzl@protonmail.ch>
2025-05-10 18:16:39 +0200

    rename aux statements to auxiliary

--- src/bloqade/stim/__init__.py
+++ src/bloqade/stim/__init__.py
@@ -1,6 +1,6 @@
 from .groups import main as main
 from ._wrappers import *  # noqa: F403
-from .dialects.aux import *  # noqa F403
 from .dialects.gate import *  # noqa F403
 from .dialects.noise import *  # noqa F403
 from .dialects.collapse import *  # noqa F403
+from .dialects.auxiliary import *  # noqa F403
