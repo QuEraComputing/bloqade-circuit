0381e5d920a7f05039696225297c82899eda3abb
John Long <johnzl@protonmail.ch>
2025-05-13 17:45:54 -0400

    remove block comment, methodtable has methods no longer needed because of removal of certain statements

--- src/bloqade/pyqrack/squin/qubit.py
+++ src/bloqade/pyqrack/squin/qubit.py
@@ -59,30 +59,3 @@ class PyQrackMethods(interp.MethodTable):
         qbit: PyQrackQubit = frame.get(stmt.qubit)
         result = self._measure_qubit(qbit)
         return (result,)
-
-
-"""
-    @interp.impl(qubit.MeasureAndReset)
-    def measure_and_reset(
-        self,
-        interp: PyQrackInterpreter,
-        frame: interp.Frame,
-        stmt: qubit.MeasureAndReset,
-    ):
-        qubits: ilist.IList[PyQrackQubit, Any] = frame.get(stmt.qubits)
-        result = []
-        for qbit in qubits:
-            if qbit.is_active():
-                result.append(qbit.sim_reg.m(qbit.addr))
-            else:
-                result.append(None)
-            qbit.sim_reg.force_m(qbit.addr, 0)
-
-        return (ilist.IList(result),)
-
-    @interp.impl(qubit.Reset)
-    def reset(self, interp: PyQrackInterpreter, frame: interp.Frame, stmt: qubit.Reset):
-        qubits: ilist.IList[PyQrackQubit, Any] = frame.get(stmt.qubits)
-        for qbit in qubits:
-            qbit.sim_reg.force_m(qbit.addr, 0)
-"""
