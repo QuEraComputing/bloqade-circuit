ed565bbd827c6585dde357a4d982e7eb4712c257
kaihsin <kaihsinwu@gmail.com>
2025-05-16 18:04:40 -0400

    add noise emit

--- src/bloqade/qasm2/dialects/noise.py
+++ src/bloqade/qasm2/dialects/noise.py
@@ -1,6 +1,8 @@
 from kirin import ir, types, lowering
 from kirin.decl import info, statement
-
+from kirin import interp
+from bloqade.qasm2.parse import ast
+from bloqade.qasm2.emit.gate import EmitQASM2Gate, EmitQASM2Frame
 from bloqade.qasm2.types import QubitType
 
 dialect = ir.Dialect("qasm2.noise")
@@ -14,3 +16,26 @@ class Pauli1(ir.Statement):
     py: ir.SSAValue = info.argument(types.Float)
     pz: ir.SSAValue = info.argument(types.Float)
     qarg: ir.SSAValue = info.argument(QubitType)
+
+
+
+
+
+@dialect.register(key="emit.qasm2.gate")
+class Noise(interp.MethodTable):
+
+    @interp.impl(Pauli1)
+    def emit_pauli(
+        self,
+        emit: EmitQASM2Gate,
+        frame: EmitQASM2Frame,
+        stmt: Pauli1,
+    ):
+        
+        px: ast.Number = frame.get(stmt.px)
+        py: ast.Number = frame.get(stmt.py)
+        pz: ast.Number = frame.get(stmt.pz)
+        qarg: ast.Bit = frame.get(stmt.qarg)
+        
+        frame.body.append(ast.Comment(text=f"noist.Pauli1({px.value}, {py.value}, {pz.value}) {qarg.name}[{qarg.addr}]"))
+        return ()
