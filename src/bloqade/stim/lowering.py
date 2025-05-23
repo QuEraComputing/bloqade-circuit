ae7494017c7ecd1dcbf03e2c277f31241b041275
kaihsin <kaihsinwu@gmail.com>
2025-05-23 01:57:10 -0400

    refactor lowering

--- src/bloqade/stim/lowering.py
+++ src/bloqade/stim/lowering.py
@@ -17,6 +17,7 @@ if TYPE_CHECKING:
 Node = Union["stim.Circuit", "stim.CircuitInstruction", "stim.GateTarget"]
 LiteralType = Union[bool, int, float, str]
 
+
 def loads(
     stim_str: str,
     *,
@@ -171,7 +172,9 @@ class Stim(lowering.LoweringABC[Node]):
 
             return frame
 
-    def lower_literal(self, state: lowering.State[Node], value: LiteralType) -> ir.SSAValue:
+    def lower_literal(
+        self, state: lowering.State[Node], value: LiteralType
+    ) -> ir.SSAValue:
         match value:
             case bool():
                 stmt = auxiliary.ConstBool(value=value)
@@ -209,7 +212,9 @@ class Stim(lowering.LoweringABC[Node]):
                     f"Unexpected stim node: {type(node)} ({node!r})"
                 )
 
-    def _get_qubit_targets_ssa(self, state: lowering.State[Node], node : Node, targets: list[Node]):
+    def _get_qubit_targets_ssa(
+        self, state: lowering.State[Node], node: Node, targets: list[Node]
+    ):
         out = tuple(
             self.lower_literal(state, targ.qubit_value)
             for targ in targets
@@ -221,7 +226,9 @@ class Stim(lowering.LoweringABC[Node]):
             )
         return out
 
-    def _get_rec_targets_ssa(self, state: lowering.State[Node], node : Node, targets: list[Node]):
+    def _get_rec_targets_ssa(
+        self, state: lowering.State[Node], node: Node, targets: list[Node]
+    ):
         def make_record(id_val):
             lit = self.lower_literal(state, id_val)
             stmt = auxiliary.GetRecord(id=lit)
@@ -239,112 +246,167 @@ class Stim(lowering.LoweringABC[Node]):
             )
         return out
 
-
-    def _get_float_args_ssa(self, state: lowering.State[Node], gate_args: list[LiteralType]):
+    def _get_float_args_ssa(
+        self, state: lowering.State[Node], gate_args: list[LiteralType]
+    ):
         return tuple(self.lower_literal(state, val) for val in gate_args)
 
-    def _get_optional_float_arg_ssa(self, state: lowering.State[Node], gate_args: list[LiteralType]):
+    def _get_optional_float_arg_ssa(
+        self, state: lowering.State[Node], gate_args: list[LiteralType]
+    ):
         val = float(gate_args[0]) if len(gate_args) >= 1 else 0.0
         return self.lower_literal(state, val)
 
-    def _get_optional_int_arg_ssa(self, state: lowering.State[Node], gate_args: list[LiteralType]):
+    def _get_optional_int_arg_ssa(
+        self, state: lowering.State[Node], gate_args: list[LiteralType]
+    ):
         val = int(gate_args[0]) if len(gate_args) >= 1 else 0
         return self.lower_literal(state, val)
 
+    # Stim gates defined here: https://github.com/quantumlib/Stim/blob/main/doc/gates.md
+
+    def _visit_reset(
+        self, state: lowering.State[Node], name: str, node
+    ) -> ir.Statement:
+        return getattr(collapse, name)(
+            targets=self._get_qubit_targets_ssa(state, node, node.targets_copy())
+        )
 
     def visit_RZ(self, state: lowering.State[Node], node: "stim.RZ") -> ir.Statement:
-        return stmt = collapse.RZ(targets=self._get_qubit_targets_ssa())
+        return self._visit_reset(state, "RZ", node)
 
+    def visit_RX(self, state: lowering.State[Node], node: "stim.RX") -> ir.Statement:
+        return self._visit_reset(state, "RX", node)
+
+    def visit_RY(self, state: lowering.State[Node], node: "stim.RY") -> ir.Statement:
+        return self._visit_reset(state, "RY", node)
+
+    def visit_TICK(
+        self, state: lowering.State[Node], node: "stim.TICK"
+    ) -> ir.Statement:
+        return auxiliary.Tick()
+
+    def _visit_measure(
+        self, state: lowering.State[Node], name: str, node
+    ) -> ir.Statement:
+        return getattr(collapse, name)(
+            p=self._get_optional_float_arg_ssa(state, node.gate_args_copy()),
+            targets=self._get_qubit_targets_ssa(state, node, node.targets_copy()),
+        )
+
+    def visit_MX(self, state: lowering.State[Node], node: "stim.MX") -> ir.Statement:
+        return self._visit_measure(state, "MX", node)
+
+    def visit_MY(self, state: lowering.State[Node], node: "stim.MY") -> ir.Statement:
+        return self._visit_measure(state, "MY", node)
+
+    def visit_MZ(self, state: lowering.State[Node], node: "stim.MZ") -> ir.Statement:
+        return self._visit_measure(state, "MZ", node)
+
+    def visit_MXX(self, state: lowering.State[Node], node: "stim.MXX") -> ir.Statement:
+        return self._visit_measure(state, "MXX", node)
+
+    def visit_MYY(self, state: lowering.State[Node], node: "stim.MYY") -> ir.Statement:
+        return self._visit_measure(state, "MYY", node)
+
+    def visit_MZZ(self, state: lowering.State[Node], node: "stim.MZZ") -> ir.Statement:
+        return self._visit_measure(state, "MZZ", node)
+
+    def visit_DETECTOR(
+        self, state: lowering.State[Node], node: "stim.Detector"
+    ) -> ir.Statement:
+        return auxiliary.Detector(
+            coord=self._get_float_args_ssa(state, node.gate_args_copy()),
+            targets=self._get_rec_targets_ssa(state, node, node.targets_copy()),
+        )
+
+    def visit_OBSERVABLE_INCLUDE(
+        self, state: lowering.State[Node], node: "stim.ObservableInclude"
+    ) -> ir.Statement:
+        return auxiliary.ObservableInclude(
+            idx=self._get_optional_int_arg_ssa(state, node.gate_args_copy()),
+            targets=self._get_rec_targets_ssa(state, node, node.targets_copy()),
+        )
+
+    def visit_I_ERROR(
+        self, state: lowering.State[Node], node: "stim.CircuitInstruction"
+    ) -> ir.Statement:
+        # I_ERROR represents any noise supported by external simulators but not stim
+        # Parse tag
+        tag_parts = node.tag.split(";", maxsplit=1)[0].split(":", maxsplit=1)
+        nonstim_name = tag_parts[0]
+        nonce = 0
+        if len(tag_parts) == 2:
+            try:
+                nonce = int(tag_parts[1])
+            except ValueError:
+                # String was not an integer
+                if self.error_unknown_nonstim:
+                    raise lowering.BuildError(
+                        f"Unsupported non-stim tag format: {node.tag!r} ({node!r})"
+                    )
+                return
+        if nonstim_name not in self.nonstim_noise_ops and self.error_unknown_nonstim:
+            raise lowering.BuildError(
+                f"Unknown non-stim statement name: {nonstim_name!r} ({node!r})"
+            )
+        statement_cls = self.nonstim_noise_ops.get(nonstim_name)
+        if statement_cls is not None:
+            if issubclass(statement_cls, noise.NonStimCorrelatedError):
+                stmt = statement_cls(
+                    nonce=nonce,
+                    probs=self._get_float_args_ssa(state, node.gate_args_copy()),
+                    targets=self._get_rec_targets_ssa(state, node, node.targets_copy()),
+                )
+            else:
+                stmt = statement_cls(
+                    probs=self._get_float_args_ssa(state, node.gate_args_copy()),
+                    targets=self._get_rec_targets_ssa(state, node, node.targets_copy()),
+                )
+        return stmt
 
     def visit_CircuitInstruction(
         self, state: lowering.State[Node], node: "stim.CircuitInstruction"
     ) -> lowering.Result:
         name = node.name.upper()
-        gate_args = node.gate_args_copy()
-        targets = node.targets_copy()
-        _target_groups = node.target_groups()  # Uncommonly used by instructions
-        _num_measurements = node.num_measurements  # Uncommonly used by instructions
-        tag = (
-            node.tag
-        )  # Used to store non-stim information about the circuit instruction
+        # gate_args = node.gate_args_copy()
+        # targets = node.targets_copy()
+        # _target_groups = node.target_groups()  # Uncommonly used by instructions
+        # _num_measurements = node.num_measurements  # Uncommonly used by instructions
+        # tag = (
+        #     node.tag
+        # )  # Used to store non-stim information about the circuit instruction
 
-        stmt = None
         match name:
             # Stim name abbreviation substitutions to canonical name
             case "R":
                 name = "RZ"
             case "M":
                 name = "MZ"
-        match name:
-            # Stim gates defined here: https://github.com/quantumlib/Stim/blob/main/doc/gates.md
-            case "QUBIT_COORDS":
-                # TODO: Add the QubitCoords instruction to the stim dialect
-                # stmt = auxiliary.QubitCoords(
-                #    coords=get_float_args_ssa(),
-                #    targets=self._get_qubit_targets_ssa(),
-                # )
-                pass
-            case "TICK":
-                stmt = auxiliary.Tick()
-            case "RX" | "RY" | "RZ":
-                stmt = getattr(collapse, name)(targets=self._get_qubit_targets_ssa())
-            # TODO: Add many more stim gates...
-            # ...
-            # ...
-            case "MX" | "MY" | "MZ" | "MXX" | "MYY" | "MZZ":
-                stmt = getattr(collapse, name)(
-                    p=get_optional_float_arg_ssa(),
-                    targets=self._get_qubit_targets_ssa(),
+
+        # dispatch base on name (capital)
+        inst = getattr(self, f"visit_{name}", None)
+        if inst is not None:
+            stmt = inst(state, node)
+            if stmt is not None:
+                state.current_frame.push(stmt)
+        else:
+            if not self.ignore_unknown_stim:
+                raise lowering.BuildError(
+                    f"Unsupported stim instruction: {type(node)} ({node!r})"
                 )
-            case "DETECTOR":
-                stmt = auxiliary.Detector(
-                    coord=get_float_args_ssa(),
-                    targets=get_rec_targets_ssa(),
-                )
-            case "OBSERVABLE_INCLUDE":
-                stmt = auxiliary.ObservableInclude(
-                    idx=get_optional_int_arg_ssa(),
-                    targets=get_rec_targets_ssa(),
-                )
-            case "I_ERROR":
-                # I_ERROR represents any noise supported by external simulators but not stim
-                # Parse tag
-                tag_parts = tag.split(";", maxsplit=1)[0].split(":", maxsplit=1)
-                nonstim_name = tag_parts[0]
-                nonce = 0
-                if len(tag_parts) == 2:
-                    try:
-                        nonce = int(tag_parts[1])
-                    except ValueError:
-                        # String was not an integer
-                        if self.error_unknown_nonstim:
-                            raise lowering.BuildError(
-                                f"Unsupported non-stim tag format: {tag!r} ({node!r})"
-                            )
-                        return
-                if (
-                    nonstim_name not in self.nonstim_noise_ops
-                    and self.error_unknown_nonstim
-                ):
-                    raise lowering.BuildError(
-                        f"Unknown non-stim statement name: {nonstim_name!r} ({node!r})"
-                    )
-                statement_cls = self.nonstim_noise_ops.get(nonstim_name)
-                if statement_cls is not None:
-                    if issubclass(statement_cls, noise.NonStimCorrelatedError):
-                        stmt = statement_cls(
-                            nonce=nonce,
-                            probs=get_float_args_ssa(),
-                            targets=self._get_qubit_targets_ssa(),
-                        )
-                    else:
-                        stmt = statement_cls(
-                            probs=get_float_args_ssa(), targets=self._get_qubit_targets_ssa()
-                        )
-            case _:
-                if not self.ignore_unknown_stim:
-                    raise lowering.BuildError(
-                        f"Unsupported stim instruction: {type(node)} ({node!r})"
-                    )
-        if stmt is not None:
-            state.current_frame.push(stmt)
+
+        # -----
+        # match name:
+        #     # Stim gates defined here: https://github.com/quantumlib/Stim/blob/main/doc/gates.md
+        #     case "QUBIT_COORDS":
+        #         # TODO: Add the QubitCoords instruction to the stim dialect
+        #         # stmt = auxiliary.QubitCoords(
+        #         #    coords=get_float_args_ssa(),
+        #         #    targets=self._get_qubit_targets_ssa(),
+        #         # )
+        #         pass
+
+        #     # TODO: Add many more stim gates...
+        #     # ...
+        #     # ...
