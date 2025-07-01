7e25fc2eb94aa995fa34cf09bc23223d651895b4
David Plankensteiner <david-pl@users.noreply.github.com>
2025-07-01 17:37:02 +0200

    Allow passing in and returning the quantum register when loading cirq.Circuits into squin (#313)
    While looking into this I realized that the only thing that makes sense
    (IMO) to pass into or return from a circuit is the list of qubits. I
    added two simple keyword arguments to control the behavior.
    
    The result is that you can compose kernels that you can easily compose
    kernels that you generate from circuits. For example:
    
    ```python
    q = cirq.LineQubit.range(2)
    circuit = cirq.Circuit(cirq.H(q[0]), cirq.CX(*q))
    
    get_entangled_qubits = squin.cirq.load_circuit(
        circuit, return_register=True, kernel_name="get_entangled_qubits"
    )
    get_entangled_qubits.print()
    
    entangle_qubits = squin.cirq.load_circuit(
        circuit, register_as_argument=True, kernel_name="entangle_qubits"
    )
    
    @squin.kernel
    def main():
        qreg = get_entangled_qubits()
        qreg2 = squin.qubit.new(1)
        entangle_qubits([qreg[1], qreg2[0]])
        return squin.qubit.measure(qreg2)
    ```
    
    Here, `get_entangled_qubits` allocates a new register, entangles it and
    returns the result, whereas `entangle_qubits` accepts a register of two
    qubits to entangle. Of course, you could also pass in and return from
    the same kernel by setting both `return_register=True` and
    `register_as_argument=True`.
    
    FYI, @jon-wurtz , let me know if this covers the use case you had in
    mind.
    
    Closes #302

--- src/bloqade/squin/cirq/__init__.py
+++ src/bloqade/squin/cirq/__init__.py
@@ -18,6 +18,9 @@ def load_circuit(
     circuit: cirq.Circuit,
     kernel_name: str = "main",
     dialects: ir.DialectGroup = kernel,
+    register_as_argument: bool = False,
+    return_register: bool = False,
+    register_argument_name: str = "q",
     globals: dict[str, Any] | None = None,
     file: str | None = None,
     lineno_offset: int = 0,
@@ -32,13 +35,23 @@ def load_circuit(
     Keyword Args:
         kernel_name (str): The name of the kernel to load. Defaults to "main".
         dialects (ir.DialectGroup | None): The dialects to use. Defaults to `squin.kernel`.
+        register_as_argument (bool): Determine whether the resulting kernel function should accept
+            a single `ilist.IList[Qubit, Any]` argument that is a list of qubits used within the
+            function. This allows you to compose kernel functions generated from circuits.
+            Defaults to `False`.
+        return_register (bool): Determine whether the resulting kernel functionr returns a
+            single value of type `ilist.IList[Qubit, Any]` that is the list of qubits used
+            in the kernel function. Useful when you want to compose multiple kernel functions
+            generated from circuits. Defaults to `False`.
+        register_argument_name (str): The name of the argument that represents the qubit register.
+            Only used when `register_as_argument=True`. Defaults to "q".
         globals (dict[str, Any] | None): The global variables to use. Defaults to None.
         file (str | None): The file name for error reporting. Defaults to None.
         lineno_offset (int): The line number offset for error reporting. Defaults to 0.
         col_offset (int): The column number offset for error reporting. Defaults to 0.
         compactify (bool): Whether to compactify the output. Defaults to True.
 
-    Example:
+    ## Usage Examples:
 
     ```python
     # from cirq's "hello qubit" example
@@ -60,6 +73,30 @@ def load_circuit(
     # print the resulting IR
     main.print()
     ```
+
+    You can also compose kernel functions generated from circuits by passing in
+    and / or returning the respective quantum registers:
+
+    ```python
+    q = cirq.LineQubit.range(2)
+    circuit = cirq.Circuit(cirq.H(q[0]), cirq.CX(*q))
+
+    get_entangled_qubits = squin.cirq.load_circuit(
+        circuit, return_register=True, kernel_name="get_entangled_qubits"
+    )
+    get_entangled_qubits.print()
+
+    entangle_qubits = squin.cirq.load_circuit(
+        circuit, register_as_argument=True, kernel_name="entangle_qubits"
+    )
+
+    @squin.kernel
+    def main():
+        qreg = get_entangled_qubits()
+        qreg2 = squin.qubit.new(1)
+        entangle_qubits([qreg[1], qreg2[0]])
+        return squin.qubit.measure(qreg2)
+    ```
     """
 
     target = Squin(dialects=dialects, circuit=circuit)
@@ -71,16 +108,38 @@ def load_circuit(
         lineno_offset=lineno_offset,
         col_offset=col_offset,
         compactify=compactify,
+        register_as_argument=register_as_argument,
+        register_argument_name=register_argument_name,
     )
 
-    # NOTE: no return value
-    return_value = func.ConstantNone()
-    body.blocks[0].stmts.append(return_value)
-    body.blocks[0].stmts.append(func.Return(value_or_stmt=return_value))
+    if return_register:
+        return_value = target.qreg
+    else:
+        return_value = func.ConstantNone()
+        body.blocks[0].stmts.append(return_value)
+
+    return_node = func.Return(value_or_stmt=return_value)
+    body.blocks[0].stmts.append(return_node)
+
+    self_arg_name = kernel_name + "_self"
+    arg_names = [self_arg_name]
+    if register_as_argument:
+        args = (target.qreg.type,)
+        arg_names.append(register_argument_name)
+    else:
+        args = ()
+
+    # NOTE: add _self as argument; need to know signature before so do it after lowering
+    signature = func.Signature(args, return_node.value.type)
+    body.blocks[0].args.insert_from(
+        0,
+        types.Generic(ir.Method, types.Tuple.where(signature.inputs), signature.output),
+        self_arg_name,
+    )
 
     code = func.Function(
         sym_name=kernel_name,
-        signature=func.Signature((), types.NoneType),
+        signature=signature,
         body=body,
     )
 
@@ -88,7 +147,7 @@ def load_circuit(
         mod=None,
         py_func=None,
         sym_name=kernel_name,
-        arg_names=[],
+        arg_names=arg_names,
         dialects=dialects,
         code=code,
     )
