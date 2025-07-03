9af9572972f1f6a389ef9ebe6943cc1b871a9758
E Huang <ehua7365@users.noreply.github.com>
2025-07-03 17:08:30 -0400

    Add squin to stim rewrite for 2Q Pauli noise

--- src/bloqade/stim/rewrite/squin_noise.py
+++ src/bloqade/stim/rewrite/squin_noise.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Tuple
 from dataclasses import dataclass
 
 from kirin.ir import SSAValue, Statement
@@ -34,13 +34,23 @@ class SquinNoiseToStim(RewriteRule):
         # this is an SSAValue, need it to be the actual operator
         applied_op = stmt.operator.owner
 
-        if isinstance(applied_op, squin_noise.stmts.SingleQubitPauliChannel):
-            stim_stmt = self.rewrite_SingleQubitPauliChannel(stmt)
+        if isinstance(applied_op, squin_noise.stmts.NoiseChannel):
+
+            qubit_idx_ssas = insert_qubit_idx_after_apply(stmt=stmt)
+            if qubit_idx_ssas is None:
+                return RewriteResult()
+
+            stim_stmt = None
+            if isinstance(applied_op, squin_noise.stmts.SingleQubitPauliChannel):
+                stim_stmt = self.rewrite_SingleQubitPauliChannel(stmt, qubit_idx_ssas)
+            elif isinstance(applied_op, squin_noise.stmts.TwoQubitPauliChannel):
+                stim_stmt = self.rewrite_TwoQubitPauliChannel(stmt, qubit_idx_ssas)
 
             if isinstance(stmt, (wire.Apply, wire.Broadcast)):
                 create_wire_passthrough(stmt)
 
-            stmt.replace_by(stim_stmt)
+            if stim_stmt is not None:
+                stmt.replace_by(stim_stmt)
             if len(stmt.operator.owner.result.uses) == 0:
                 stmt.operator.owner.delete()
 
@@ -50,15 +60,12 @@ class SquinNoiseToStim(RewriteRule):
     def rewrite_SingleQubitPauliChannel(
         self,
         stmt: qubit.Apply | qubit.Broadcast | wire.Broadcast | wire.Apply,
+        qubit_idx_ssas: Tuple[SSAValue],
     ) -> Statement:
         """Rewrite squin.noise.SingleQubitPauliChannel to stim.PauliChannel1."""
 
         squin_channel = stmt.operator.owner
         assert isinstance(squin_channel, squin_noise.stmts.SingleQubitPauliChannel)
-
-        qubit_idx_ssas = insert_qubit_idx_after_apply(stmt=stmt)
-        if qubit_idx_ssas is None:
-            return RewriteResult()
 
         params = self.cp_results.get(squin_channel.params).data
         new_stmts = [
@@ -74,5 +81,40 @@ class SquinNoiseToStim(RewriteRule):
             px=p_x.result,
             py=p_y.result,
             pz=p_z.result,
+        )
+        return stim_stmt
+
+    def rewrite_TwoQubitPauliChannel(
+        self,
+        stmt: qubit.Apply | qubit.Broadcast | wire.Broadcast | wire.Apply,
+        qubit_idx_ssas: Tuple[SSAValue],
+    ) -> Statement:
+        """Rewrite squin.noise.SingleQubitPauliChannel to stim.PauliChannel1."""
+
+        squin_channel = stmt.operator.owner
+        assert isinstance(squin_channel, squin_noise.stmts.TwoQubitPauliChannel)
+
+        params = self.cp_results.get(squin_channel.params).data
+        param_stmts = [py.Constant(p) for p in params]
+        for param_stmt in param_stmts:
+            param_stmt.insert_before(stmt)
+
+        stim_stmt = stim_noise.PauliChannel2(
+            targets=qubit_idx_ssas,
+            pix=param_stmts[0].result,
+            piy=param_stmts[1].result,
+            piz=param_stmts[2].result,
+            pxi=param_stmts[3].result,
+            pxx=param_stmts[4].result,
+            pxy=param_stmts[5].result,
+            pxz=param_stmts[6].result,
+            pyi=param_stmts[7].result,
+            pyx=param_stmts[8].result,
+            pyy=param_stmts[9].result,
+            pyz=param_stmts[10].result,
+            pzi=param_stmts[11].result,
+            pzx=param_stmts[12].result,
+            pzy=param_stmts[13].result,
+            pzz=param_stmts[14].result,
         )
         return stim_stmt
--- test/stim/passes/test_squin_noise_to_stim.py
+++ test/stim/passes/test_squin_noise_to_stim.py
@@ -67,3 +67,40 @@ def test_broadcast_pauli_channel_1_reuse():
             "PAULI_CHANNEL_1(0.01000000, 0.02000000, 0.03000000) 0",
         ]
     )
+
+
+def test_broadcast_pauli_channel_2():
+
+    @kernel
+    def test():
+        q = qubit.new(1)
+        channel = noise.two_qubit_pauli_channel(
+            params=[
+                0.001,
+                0.002,
+                0.003,
+                0.004,
+                0.005,
+                0.006,
+                0.007,
+                0.008,
+                0.009,
+                0.010,
+                0.011,
+                0.012,
+                0.013,
+                0.014,
+                0.015,
+            ]
+        )
+        qubit.broadcast(channel, q)
+        return
+
+    run_address_and_stim_passes(test)
+    assert codegen(test).strip() == (
+        "PAULI_CHANNEL_2("
+        "0.00100000, 0.00200000, 0.00300000, 0.00400000, 0.00500000, "
+        "0.00600000, 0.00700000, 0.00800000, 0.00900000, 0.01000000, "
+        "0.01100000, 0.01200000, 0.01300000, 0.01400000, 0.01500000"
+        ") 0"
+    )
