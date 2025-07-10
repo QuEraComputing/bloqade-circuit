a95c4d2bd9820d1904a2f149bcfc4c3d23c49f34
John Long <johnzl@protonmail.ch>
2025-07-10 14:01:33 -0400

    add incomplete measure id test on invoke

--- test/analysis/measure_id/test_measure_id.py
+++ test/analysis/measure_id/test_measure_id.py
@@ -1,3 +1,6 @@
+# from kirin.types import AnyType
+# from kirin.dialects.ilist import IList
+
 from bloqade.squin import op, qubit, kernel
 from bloqade.analysis.measure_id import MeasurementIDAnalysis
 from bloqade.analysis.measure_id.lattice import MeasureIdBool, MeasureIdTuple
@@ -26,3 +29,27 @@ def test_add():
         data=tuple([MeasureIdBool(idx=i) for i in range(1, 11)])
     )
     assert measure_id_tuples[-1] == expected_measure_id_tuple
+
+
+# Not sure why the measure_qubit_list impl doesn't get
+# called considering the invoke impl does...
+def test_invoke():
+
+    @kernel
+    # If I put an explicit type here it becomes AnyMeasureId
+    # and it does touch the right impl but
+    # the type attribute data is missing the list length
+    def measure_generating_func(ql):
+        return qubit.measure(ql)
+
+    @kernel
+    def test():
+        ql = qubit.new(5)
+        qubit.broadcast(op.x(), ql)
+        return measure_generating_func(ql)
+
+    frame, _ = MeasurementIDAnalysis(test.dialects).run_analysis(test)
+
+    # measure_generating_func.print(analysis=frame.entries)
+
+    test.print(analysis=frame.entries)
