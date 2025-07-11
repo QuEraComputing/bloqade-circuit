b31415d36c719af4cffc6534ead3ea83dc3f2571
John Long <johnzl@protonmail.ch>
2025-07-11 11:51:16 -0400

    remove unnecessary invoke test for measure_id

--- test/analysis/measure_id/test_measure_id.py
+++ test/analysis/measure_id/test_measure_id.py
@@ -29,27 +29,3 @@ def test_add():
         data=tuple([MeasureIdBool(idx=i) for i in range(1, 11)])
     )
     assert measure_id_tuples[-1] == expected_measure_id_tuple
-
-
-# Not sure why the measure_qubit_list impl doesn't get
-# called considering the invoke impl does...
-def test_invoke():
-
-    @kernel
-    # If I put an explicit type here it becomes AnyMeasureId
-    # and it does touch the right impl but
-    # the type attribute data is missing the list length
-    def measure_generating_func(ql):
-        return qubit.measure(ql)
-
-    @kernel
-    def test():
-        ql = qubit.new(5)
-        qubit.broadcast(op.x(), ql)
-        return measure_generating_func(ql)
-
-    frame, _ = MeasurementIDAnalysis(test.dialects).run_analysis(test)
-
-    # measure_generating_func.print(analysis=frame.entries)
-
-    test.print(analysis=frame.entries)
