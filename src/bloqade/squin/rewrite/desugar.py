0c50cc2039fb91a19992edb10d8797297e059cec
David Plankensteiner <da.plankensteiner@gmail.com>
2025-07-24 10:51:56 +0200

    Simplify desugar pass based on kirin fix

--- src/bloqade/squin/rewrite/desugar.py
+++ src/bloqade/squin/rewrite/desugar.py
@@ -46,8 +46,7 @@ class ApplyDesugarRule(RewriteRule):
     """
     Desugar apply operators in the kernel.
 
-    NOTE: this pass can be removed once we are at kirin v0.18 and we decide to
-    disallow the syntax apply(op: Op, qubits: list)
+    NOTE: this pass can be removed once we decide to disallow the syntax apply(op: Op, qubits: list)
     """
 
     def rewrite_Statement(self, node: ir.Statement) -> RewriteResult:
@@ -58,65 +57,46 @@ class ApplyDesugarRule(RewriteRule):
         op = node.operator
         qubits = node.qubits
 
+        if len(qubits) == 0:
+            # NOTE: this is invalid syntax, but we don't error in rewrites
+            return RewriteResult()
+
         if all(q.type.is_subseteq(QubitType) for q in qubits):
+            # NOTE: this is the syntax we want; the entire rewrite becomes unnecessary
+            # once we disallow the old syntax (just wrap Apply directly)
             apply_stmt = Apply(op, qubits)
             node.replace_by(apply_stmt)
             return RewriteResult(has_done_something=True)
 
-        if len(qubits) == 1 and (qubit_type := qubits[0].type).is_subseteq(
-            ilist.IListType[QubitType, types.Any]
-        ):
-            warn(
-                "The syntax `apply(operator: Op, qubits: list[Qubit])` is deprecated and may already lead to errors. Use `apply(operator: Op, *qubits: Qubit)` instead."
-            )
-            # NOTE: deprecated syntax
-            # TODO: remove one we disallow apply(op: Op, q: ilist.IList[Qubit])
-            if not isinstance(qubit_type.vars[1], types.Literal):
-                # NOTE: unknown size, nothing we can do here, it will probably error down the road somewhere
-                return RewriteResult()
+        if len(qubits) > 1:
+            # NOTE: multiple arguments, that aren't qubits, let's bail
+            return RewriteResult()
 
-            n = qubit_type.vars[1].data
-            if not isinstance(n, int):
-                # wat?
-                return RewriteResult()
+        qubit_type = qubits[0].type
+        is_qubit_list = qubit_type.is_subseteq(ilist.IListType[QubitType, types.Any])
 
-            qubits_rewrite = []
-            for i in range(n):
-                (idx := py.Constant(i)).insert_before(node)
-                (get_item := py.GetItem(qubits[0], idx.result)).insert_before(node)
-                qubits_rewrite.append(get_item.result)
+        if not is_qubit_list:
+            return RewriteResult()
 
-            apply_stmt = Apply(op, tuple(qubits_rewrite))
-            node.replace_by(apply_stmt)
-            return RewriteResult(has_done_something=True)
+        # NOTE: deprecated syntax: we have a single list of qubits here
+        warn(
+            "The syntax `apply(operator: Op, qubits: list[Qubit])` is deprecated and may already lead to errors. Use `apply(operator: Op, *qubits: Qubit)` instead."
+        )
+        if not isinstance(qubit_type.vars[1], types.Literal):
+            # NOTE: unknown size, nothing we can do here, it will probably error down the road somewhere
+            return RewriteResult()
 
-        if len(qubits) == 1:
-            # NOTE: we might have a single ilist with wrong typing here
-            # TODO: remove this elif clause once we're at kirin v0.18
-            # NOTE: this is a temporary workaround for kirin#408
-            # currently type inference fails here in for loops since the loop var
-            # is an IList for some reason
+        n = qubit_type.vars[1].data
+        if not isinstance(n, int):
+            # wat?
+            return RewriteResult()
 
-            if not isinstance(qubits[0], ir.ResultValue):
-                return RewriteResult()
+        qubits_rewrite = []
+        for i in range(n):
+            (idx := py.Constant(i)).insert_before(node)
+            (get_item := py.GetItem(qubits[0], idx.result)).insert_before(node)
+            qubits_rewrite.append(get_item.result)
 
-            is_ilist = isinstance(qbit_stmt := qubits[0].stmt, ilist.New)
-
-            if is_ilist:
-
-                qbit_getindices = qbit_stmt.values
-
-                if not all(
-                    isinstance(qbit_getindex_result, ir.ResultValue)
-                    for qbit_getindex_result in qbit_getindices
-                ):
-                    return RewriteResult()
-
-            else:
-                qbit_getindices = qubits
-
-            stmt = Apply(operator=op, qubits=tuple(qbit_getindices))
-            node.replace_by(stmt)
-            return RewriteResult(has_done_something=True)
-
-        return RewriteResult()
+        apply_stmt = Apply(op, tuple(qubits_rewrite))
+        node.replace_by(apply_stmt)
+        return RewriteResult(has_done_something=True)
