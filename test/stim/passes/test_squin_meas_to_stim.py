f792511c7fa5f395f0f82f28027424ec783aaa1c
John Long <jlong@quera.com>
2025-07-26 12:25:40 -0400

    get rid of now unnecessary functions

--- test/stim/passes/test_squin_meas_to_stim.py
+++ test/stim/passes/test_squin_meas_to_stim.py
@@ -38,11 +38,48 @@ def test_cond_on_measurement():
 
     SquinToStimPass(main.dialects)(main)
 
-    main.print()
-
     path = os.path.join(
         os.path.dirname(__file__), "stim_reference_programs", "simple_if_rewrite.txt"
     )
+    with open(path, "r") as f:
+        base_stim_prog = f.read()
+
+    assert base_stim_prog.rstrip() == codegen(main)
+
+
+def test_record_index_order():
+
+    @squin.kernel
+    def main():
+        n_qubits = 4
+        q = qubit.new(n_qubits)
+
+        ms0 = qubit.measure(q)
+
+        if ms0[0]:  # should be rec[-4]
+            qubit.apply(op.z(), q[0])
+
+        # another measurement
+        ms1 = qubit.measure(q)
+
+        if ms1[0]:  # should be rec[-4]
+            qubit.apply(op.x(), q[0])
+
+        # second round of measurement
+        ms2 = qubit.measure(q)  # noqa: F841
+
+        # try accessing measurements from the very first round
+        ## There are now 12 total measurements, ms0[0]
+        ## is the oldest measurement in the entire program
+        if ms0[0]:
+            qubit.apply(op.y(), q[1])
+
+    SquinToStimPass(main.dialects)(main)
+
+    path = os.path.join(
+        os.path.dirname(__file__), "stim_reference_programs", "record_index_order.stim"
+    )
+
     with open(path, "r") as f:
         base_stim_prog = f.read()
 
