bbf04669fce9ab4d9c4621c38c602ba5a1474379
David Plankensteiner <da.plankensteiner@gmail.com>
2025-08-06 16:13:00 +0200

    Cleaner tests and fix typing for squin2stim

--- test/squin/test_all_statements.py
+++ test/squin/test_all_statements.py
@@ -5,9 +5,10 @@ from kirin.ir import Statement
 from kirin.rewrite import Walk
 from kirin.rewrite.abc import RewriteRule, RewriteResult
 
-from bloqade import squin
+from bloqade import stim, squin
 from bloqade.pyqrack import StackMemorySimulator
 from bloqade.analysis import address, measure_id
+from bloqade.stim.emit import EmitStimMain
 from bloqade.stim.passes import SquinToStimPass
 from bloqade.squin.noise.rewrite import RewriteNoiseStmts
 from bloqade.squin.analysis.nsites import NSitesAnalysis
@@ -42,115 +43,115 @@ noise_stmts = get_dialect_stmts(squin.noise, squin.noise.stmts)
 # TODO: wire(?)
 
 
-# NOTE: define the method to be used in all test top-level; DO NOT MODIFY IN TEST!
+# NOTE: define the method to be used in all test top-level
 
 
-@squin.kernel(fold=False)
-def main():
+def get_test_kernel(fold: bool = False):
+    @squin.kernel(fold=fold)
+    def main():
 
-    ### qubit
-    q = squin.qubit.new(5)
-    x = squin.op.x()
-    squin.qubit.apply(x, q[0])
-    squin.qubit.broadcast(x, q)
-    squin.qubit.measure(q[1])
-    squin.qubit.measure(q)
+        ### qubit
+        q = squin.qubit.new(5)
+        x = squin.op.x()
+        squin.qubit.apply(x, q[0])
+        squin.qubit.broadcast(x, q)
+        squin.qubit.measure(q[1])
+        squin.qubit.measure(q)
 
-    ### op
-    squin.op.kron(x, x)
-    squin.op.mult(x, x)
-    squin.op.scale(x, 1.0)
-    squin.op.adjoint(x)
-    squin.op.control(x, n_controls=1)
-    squin.op.reset()
-    squin.op.reset_to_one()
-    squin.op.identity(sites=2)
-    squin.op.rot(x, 0.123)
-    squin.op.shift(0.234)
-    squin.op.phase(0.234)
-    squin.op.x()
-    squin.op.y()
-    squin.op.z()
-    squin.op.sqrt_x()
-    squin.op.sqrt_y()
-    squin.op.sqrt_z()
-    squin.op.h()
-    squin.op.s()
-    squin.op.t()
-    squin.op.p0()
-    squin.op.p1()
-    squin.op.spin_n()
-    squin.op.spin_p()
-    squin.op.u(0.234, 1.5, 3.14159)
-    squin.op.pauli_string(string="XYZ")
+        ### op
+        squin.op.kron(x, x)
+        squin.op.mult(x, x)
+        squin.op.scale(x, 1.0)
+        squin.op.adjoint(x)
+        squin.op.control(x, n_controls=1)
+        squin.op.reset()
+        squin.op.reset_to_one()
+        squin.op.identity(sites=2)
+        squin.op.rot(x, 0.123)
+        squin.op.shift(0.234)
+        squin.op.phase(0.234)
+        squin.op.x()
+        squin.op.y()
+        squin.op.z()
+        squin.op.sqrt_x()
+        squin.op.sqrt_y()
+        squin.op.sqrt_z()
+        squin.op.h()
+        squin.op.s()
+        squin.op.t()
+        squin.op.p0()
+        squin.op.p1()
+        squin.op.spin_n()
+        squin.op.spin_p()
+        squin.op.u(0.234, 1.5, 3.14159)
+        squin.op.pauli_string(string="XYZ")
 
-    ### noise
-    squin.noise.pauli_error(x, 0.1)
-    squin.noise.depolarize(0.1)
-    squin.noise.depolarize2(0.1)
-    squin.noise.single_qubit_pauli_channel((0.1, 0.2, 0.3))
-    two_qubit_pauli_probs = [
-        0.023354967892248113,
-        0.003247463853107968,
-        0.016397706815567816,
-        0.05881521942445792,
-        0.055273193025446865,
-        0.048159912657060724,
-        0.06652684300706593,
-        0.03042882311429303,
-        0.0018064348480710812,
-        0.0037171594239370515,
-        0.05016247526449243,
-        0.037720309303735065,
-        0.022077535704784847,
-        0.04293521011314487,
-        0.01377236173895846,
-    ]
-    squin.noise.two_qubit_pauli_channel(two_qubit_pauli_probs)
-    squin.noise.qubit_loss(0.1)
+        ### noise
+        squin.noise.pauli_error(x, 0.1)
+        squin.noise.depolarize(0.1)
+        squin.noise.depolarize2(0.1)
+        squin.noise.single_qubit_pauli_channel([0.1, 0.2, 0.3])
+        two_qubit_pauli_probs = [
+            0.023354967892248113,
+            0.003247463853107968,
+            0.016397706815567816,
+            0.05881521942445792,
+            0.055273193025446865,
+            0.048159912657060724,
+            0.06652684300706593,
+            0.03042882311429303,
+            0.0018064348480710812,
+            0.0037171594239370515,
+            0.05016247526449243,
+            0.037720309303735065,
+            0.022077535704784847,
+            0.04293521011314487,
+            0.01377236173895846,
+        ]
+        squin.noise.two_qubit_pauli_channel(two_qubit_pauli_probs)
+        squin.noise.qubit_loss(0.1)
 
-    ### apply op & noise so that the runtime is actually executed
+        ### apply op & noise so that the runtime is actually executed
 
-    ### op
-    squin.qubit.apply(squin.op.kron(x, x), q[0], q[1])
-    squin.qubit.apply(squin.op.mult(x, x), q[0], q[1])
-    squin.qubit.apply(squin.op.scale(x, 1.0), q[0])
-    squin.qubit.apply(squin.op.adjoint(x), q[0])
-    squin.qubit.apply(squin.op.control(x, n_controls=1), q[0], q[1])
-    squin.qubit.apply(squin.op.reset(), q[0])
-    squin.qubit.apply(squin.op.reset_to_one(), q[0])
-    squin.qubit.apply(squin.op.identity(sites=2), q[0], q[1])
-    squin.qubit.apply(squin.op.rot(x, 0.123), q[0])
-    squin.qubit.apply(squin.op.shift(0.234), q[0])
-    squin.qubit.apply(squin.op.phase(0.234), q[0])
-    squin.qubit.apply(squin.op.x(), q[0])
-    squin.qubit.apply(squin.op.y(), q[0])
-    squin.qubit.apply(squin.op.z(), q[0])
-    squin.qubit.apply(squin.op.sqrt_x(), q[0])
-    squin.qubit.apply(squin.op.sqrt_y(), q[0])
-    squin.qubit.apply(squin.op.sqrt_z(), q[0])
-    squin.qubit.apply(squin.op.h(), q[0])
-    squin.qubit.apply(squin.op.s(), q[0])
-    squin.qubit.apply(squin.op.t(), q[0])
-    squin.qubit.apply(squin.op.p0(), q[0])
-    squin.qubit.apply(squin.op.p1(), q[0])
-    squin.qubit.apply(squin.op.spin_n(), q[0])
-    squin.qubit.apply(squin.op.spin_p(), q[0])
-    squin.qubit.apply(squin.op.u(0.234, 1.5, 3.14159), q[0])
-    squin.qubit.apply(squin.op.pauli_string(string="XYZ"), q[0], q[1], q[2])
+        ### op
+        squin.qubit.apply(squin.op.kron(x, x), q[0], q[1])
+        squin.qubit.apply(squin.op.mult(x, x), q[0], q[1])
+        squin.qubit.apply(squin.op.scale(x, 1.0), q[0])
+        squin.qubit.apply(squin.op.adjoint(x), q[0])
+        squin.qubit.apply(squin.op.control(x, n_controls=1), q[0], q[1])
+        squin.qubit.apply(squin.op.reset(), q[0])
+        squin.qubit.apply(squin.op.reset_to_one(), q[0])
+        squin.qubit.apply(squin.op.identity(sites=2), q[0], q[1])
+        squin.qubit.apply(squin.op.rot(x, 0.123), q[0])
+        squin.qubit.apply(squin.op.shift(0.234), q[0])
+        squin.qubit.apply(squin.op.phase(0.234), q[0])
+        squin.qubit.apply(squin.op.x(), q[0])
+        squin.qubit.apply(squin.op.y(), q[0])
+        squin.qubit.apply(squin.op.z(), q[0])
+        squin.qubit.apply(squin.op.sqrt_x(), q[0])
+        squin.qubit.apply(squin.op.sqrt_y(), q[0])
+        squin.qubit.apply(squin.op.sqrt_z(), q[0])
+        squin.qubit.apply(squin.op.h(), q[0])
+        squin.qubit.apply(squin.op.s(), q[0])
+        squin.qubit.apply(squin.op.t(), q[0])
+        squin.qubit.apply(squin.op.p0(), q[0])
+        squin.qubit.apply(squin.op.p1(), q[0])
+        squin.qubit.apply(squin.op.spin_n(), q[0])
+        squin.qubit.apply(squin.op.spin_p(), q[0])
+        squin.qubit.apply(squin.op.u(0.234, 1.5, 3.14159), q[0])
+        squin.qubit.apply(squin.op.pauli_string(string="XYZ"), q[0], q[1], q[2])
 
-    ### noise
-    squin.qubit.apply(squin.noise.pauli_error(x, 0.1), q[0])
-    squin.qubit.apply(squin.noise.depolarize(0.1), q[0])
-    squin.qubit.apply(squin.noise.depolarize2(0.1), q[0], q[1])
-    squin.qubit.apply(squin.noise.single_qubit_pauli_channel((0.1, 0.2, 0.3)), q[0])
-    squin.qubit.apply(
-        squin.noise.two_qubit_pauli_channel(two_qubit_pauli_probs), q[0], q[1]
-    )
-    squin.qubit.apply(squin.noise.qubit_loss(0.1), q[0])
+        ### noise
+        squin.qubit.apply(squin.noise.pauli_error(x, 0.1), q[0])
+        squin.qubit.apply(squin.noise.depolarize(0.1), q[0])
+        squin.qubit.apply(squin.noise.depolarize2(0.1), q[0], q[1])
+        squin.qubit.apply(squin.noise.single_qubit_pauli_channel([0.1, 0.2, 0.3]), q[0])
+        squin.qubit.apply(
+            squin.noise.two_qubit_pauli_channel(two_qubit_pauli_probs), q[0], q[1]
+        )
+        squin.qubit.apply(squin.noise.qubit_loss(0.1), q[0])
 
-
-main.print()
+    return main
 
 
 @dataclass
@@ -181,6 +182,7 @@ class RemoveUnsupportedStatements(RewriteRule):
 
 
 def test_all_statements_there():
+    main = get_test_kernel()
     main.verify()
 
     # NOTE: hardcoded list of exclusions which should be rewritten
@@ -225,8 +227,8 @@ def test_all_statements_there():
 
 
 def test_noise_rewrite():
-    main_ = main.similar(main.dialects)
-    RewriteNoiseStmts(main_.dialects, no_raise=False)(main_)
+    main = get_test_kernel()
+    RewriteNoiseStmts(main.dialects, no_raise=False)(main)
 
     non_rewritable_noise = {
         squin.noise.stmts.StochasticUnitaryChannel,
@@ -236,7 +238,7 @@ def test_noise_rewrite():
 
     rewritten_stmts = [
         type(stmt)
-        for stmt in main_.callable_region.blocks[0].stmts
+        for stmt in main.callable_region.blocks[0].stmts
         if stmt.dialect
         in (
             squin.qubit.dialect,
@@ -254,6 +256,7 @@ def test_noise_rewrite():
 
 
 def test_pyqrack():
+    main = get_test_kernel()
     sim = StackMemorySimulator(min_qubits=5)
     sim.run(main)
 
@@ -261,27 +264,36 @@ def test_pyqrack():
 
 
 def test_squin2stim():
-    main_ = main.similar(main.dialects)
+    main = get_test_kernel(fold=True)
 
-    SquinToStimPass(main_.dialects, no_raise=False).fixpoint(main_)
+    # TDO: remove unsupported statements
 
-    main_.print()
+    dialects = main.dialects.add(stim.dialects.auxiliary.dialect)
+    SquinToStimPass(dialects, no_raise=False).fixpoint(main)
+
+    main.print()
+    main.verify()
+
+    stim_program = EmitStimMain(dialects).run(main, args=())
+    print(stim_program)
 
 
 def test_cirq_emit():
+    main = get_test_kernel()
+
     # NOTE: hardcoded list of statements that are not supported by cirq
     unsupported_by_cirq = (squin.noise.stmts.QubitLoss,)
 
-    main_ = main.similar(main.dialects)
     rw = Walk(RemoveUnsupportedStatements(unsupported_statements=unsupported_by_cirq))
-    rw.rewrite(main_.code)
+    rw.rewrite(main.code)
 
-    circuit = squin.cirq.emit_circuit(main_)
+    circuit = squin.cirq.emit_circuit(main)
 
     print(circuit)
 
 
 def test_address_analysis():
+    main = get_test_kernel()
     analysis = address.AddressAnalysis(main.dialects)
 
     frame, _ = analysis.run_analysis(main, no_raise=False)
@@ -290,6 +302,7 @@ def test_address_analysis():
 
 
 def test_measurement_id_analysis():
+    main = get_test_kernel()
     analysis = measure_id.analysis.MeasurementIDAnalysis(main.dialects)
 
     frame, _ = analysis.run_analysis(main, no_raise=False)
@@ -298,6 +311,7 @@ def test_measurement_id_analysis():
 
 
 def test_nsites_analysis():
+    main = get_test_kernel()
     analysis = NSitesAnalysis(main.dialects)
 
     frame, _ = analysis.run_analysis(main, no_raise=False)
