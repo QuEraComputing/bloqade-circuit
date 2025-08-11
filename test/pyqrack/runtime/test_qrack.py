4213b6fee49d5c0e6b040ddaf19fba0a767bac73
Jonathan Wurtz <jwurtz@quera.com>
2025-08-11 10:16:16 -0400

    Reduced density matrix to pyqrack (#444)
    Co-authored-by: David Plankensteiner <david-pl@users.noreply.github.com>

--- test/pyqrack/runtime/test_qrack.py
+++ test/pyqrack/runtime/test_qrack.py
@@ -1,10 +1,13 @@
 import math
 from unittest.mock import Mock, call
 
+import cirq
+import numpy as np
 from kirin import ir
 
-from bloqade import qasm2
+from bloqade import qasm2, squin
 from pyqrack.pauli import Pauli
+from bloqade.pyqrack import StackMemorySimulator
 from bloqade.pyqrack.base import MockMemory, PyQrackInterpreter
 
 
@@ -91,7 +94,6 @@ def test_u_gates():
 
 
 def test_basic_control_gates():
-
     @qasm2.main
     def program():
         q = qasm2.qreg(3)
@@ -162,3 +164,205 @@ def test_extended():
             call.r(3, 0.5, 1),
         ]
     )
+
+
+def test_rdm1():
+    """
+    Is extracting the exact state vector consistent with cirq?
+    This test also validates the ordering of the qubit basis.
+    """
+
+    @squin.kernel
+    def program():
+        q = squin.qubit.new(5)
+        squin.gate.h(q[1])
+        return q
+
+    emulator = StackMemorySimulator(min_qubits=6)
+    task = emulator.task(program)
+    qubits = task.run()
+    rho = emulator.quantum_state(qubits)
+
+    assert all(np.isclose(rho.eigenvalues, [1]))
+
+    circuit = cirq.Circuit()
+    qbs = cirq.LineQubit.range(5)
+    circuit.append(cirq.H(qbs[1]))
+    for i in range(5):
+        circuit.append(cirq.I(qbs[i]))
+    state = cirq.Simulator().simulate(circuit).state_vector()
+    assert cirq.equal_up_to_global_phase(state, rho[1][:, 0])
+
+
+def test_rdm1b():
+    """
+    Is extracting the exact state vector consistent with cirq?
+    This test also validates the ordering of the qubit basis.
+    Same as test_rdm1, but with the total qubits equal to the number of qubits in the program.
+    """
+
+    @squin.kernel
+    def program():
+        q = squin.qubit.new(5)
+        squin.gate.h(q[1])
+        return q
+
+    emulator = StackMemorySimulator(min_qubits=5)
+    task = emulator.task(program)
+    qubits = task.run()
+    rho = emulator.quantum_state(qubits)
+
+    assert all(np.isclose(rho.eigenvalues, [1]))
+
+    circuit = cirq.Circuit()
+    qbs = cirq.LineQubit.range(5)
+    circuit.append(cirq.H(qbs[1]))
+    for i in range(5):
+        circuit.append(cirq.I(qbs[i]))
+    state = cirq.Simulator().simulate(circuit).state_vector()
+    assert cirq.equal_up_to_global_phase(state, rho[1][:, 0])
+
+
+def test_rdm2():
+    """
+    Does the RDM project correctly?
+    """
+
+    @squin.kernel
+    def program():
+        """
+        Creates a GHZ state on qubits 0,1,3,4 on a total of 6 qubits.
+        """
+        q = squin.qubit.new(6)
+        squin.gate.h(q[0])
+        squin.gate.cx(q[0], q[1])
+        squin.gate.cx(q[0], q[3])
+        squin.gate.cx(q[0], q[4])
+        return q
+
+    emulator = StackMemorySimulator(min_qubits=6)
+    task = emulator.task(program)
+    qubits = task.run()
+    rho = emulator.quantum_state([qubits[x] for x in [0, 1, 3, 4]])
+    target = np.array([1] + [0] * (14) + [1]) / np.sqrt(2) + 0j
+    assert all(np.isclose(rho.eigenvalues, [1]))
+    assert cirq.equal_up_to_global_phase(rho[1][:, 0], target)
+
+    rho2 = emulator.quantum_state([qubits[x] for x in [0, 1, 3]])
+    assert all(np.isclose(rho2.eigenvalues, [0.5, 0.5]))
+    assert rho2.eigenvectors.shape == (8, 2)
+
+
+def test_rdm3():
+    """
+    Out-of-order indexing is consistent with cirq.
+    """
+
+    @squin.kernel
+    def program():
+        """
+        Random unitaries on 3 qubits.
+        """
+        q = squin.qubit.new(3)
+        squin.gate.rx(0.1, q[0])
+        squin.gate.ry(0.2, q[1])
+        squin.gate.rx(0.3, q[2])
+        return q
+
+    emulator = StackMemorySimulator(min_qubits=6)
+    task = emulator.task(program)
+    qubits = task.run()
+
+    # Canonical ordering
+    rho = emulator.quantum_state([qubits[x] for x in [0, 1, 2]])
+    circuit = cirq.Circuit()
+    qbs = cirq.LineQubit.range(3)
+    circuit.append(cirq.rx(0.1)(qbs[0]))
+    circuit.append(cirq.ry(0.2)(qbs[1]))
+    circuit.append(cirq.rx(0.3)(qbs[2]))
+    state = cirq.Simulator().simulate(circuit).state_vector()
+    assert cirq.equal_up_to_global_phase(state, rho[1][:, 0])
+
+    # Reverse ordering 0->2, 1->, 2->0
+    rho = emulator.quantum_state([qubits[x] for x in [2, 1, 0]])
+    circuit = cirq.Circuit()
+    qbs = cirq.LineQubit.range(3)
+    circuit.append(cirq.rx(0.1)(qbs[2]))
+    circuit.append(cirq.ry(0.2)(qbs[1]))
+    circuit.append(cirq.rx(0.3)(qbs[0]))
+    state = cirq.Simulator().simulate(circuit).state_vector()
+    assert cirq.equal_up_to_global_phase(state, rho[1][:, 0])
+
+    # Other ordering
+    rho = emulator.quantum_state([qubits[x] for x in [1, 2, 0]])
+    circuit = cirq.Circuit()
+    qbs = cirq.LineQubit.range(3)
+    circuit.append(cirq.rx(0.1)(qbs[2]))
+    circuit.append(cirq.ry(0.2)(qbs[0]))
+    circuit.append(cirq.rx(0.3)(qbs[1]))
+    state = cirq.Simulator().simulate(circuit).state_vector()
+    assert cirq.equal_up_to_global_phase(state, rho[1][:, 0])
+
+
+def test_rdm4():
+    rho = StackMemorySimulator.quantum_state([])
+    assert rho.eigenvalues.shape == (0,)
+    assert rho.eigenvectors.shape == (0, 0)
+
+
+def test_rdm5():
+    @squin.kernel
+    def program():
+        """
+        Random unitaries on 3 qubits.
+        """
+        q = squin.qubit.new(3)
+        return q
+
+    emulator = StackMemorySimulator(min_qubits=6)
+    task = emulator.task(program)
+    qubits = task.run()
+    rho = emulator.reduced_density_matrix(qubits)
+    assert rho.shape == (8, 8)
+
+
+def test_rdm_failures():
+    @squin.kernel
+    def program():
+        q = squin.qubit.new(3)
+        return q
+
+    emulator = StackMemorySimulator(min_qubits=6)
+    task = emulator.task(program)
+    qbsA = task.qubits()
+    qubits = task.run()
+    qubits2 = task.run()
+    qbsB = task.qubits()
+    assert len(qbsA) == 0
+    assert len(qbsB) == 6
+
+    try:
+        emulator.quantum_state([qubits[0], qubits[0]])
+        assert False, "Should have failed; qubits must be unique"
+    except ValueError as e:
+        assert str(e) == "Qubits must be unique."
+
+    try:
+        emulator.quantum_state([qubits[0], qubits2[1]])
+        assert False, "Should have failed; qubits must be from the same register"
+    except ValueError as e:
+        assert str(e) == "All qubits must be from the same simulator register."
+
+
+def test_get_qubits():
+    @squin.kernel
+    def program():
+        q = squin.qubit.new(3)
+        return q
+
+    emulator = StackMemorySimulator(min_qubits=6)
+    task = emulator.task(program)
+    task.run()
+
+    qubits2 = task.qubits()
+    assert len(qubits2) == 6
