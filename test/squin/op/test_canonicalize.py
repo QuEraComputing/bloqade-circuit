1d766ed427fd4b50135632f8e6e4698b5352689a
Phillip Weinberg <weinbe58@gmail.com>
2025-09-02 12:20:46 -0400

    simplifying test

--- test/squin/op/test_canonicalize.py
+++ test/squin/op/test_canonicalize.py
@@ -9,22 +9,18 @@ from bloqade.squin.op.rewrite import CanonicalizeAdjointRot
 
 def test_rot_canonicalize():
     angle = ir.TestValue()
+    axis = ir.TestValue()
     test_block = ir.Block()
-    test_block.stmts.append(axis := op_stmts.X())
-    test_block.stmts.append(rot := op_stmts.Rot(angle=angle, axis=axis.result))
+    test_block.stmts.append(rot := op_stmts.Rot(angle=angle, axis=axis))
     test_block.stmts.append(op_stmts.Adjoint(op=rot.result))
 
     Walk(CanonicalizeAdjointRot()).rewrite(test_block)
 
     expected_block = ir.Block()
-    expected_block.stmts.append(axis := op_stmts.X())
-    expected_block.stmts.append(rot := op_stmts.Rot(angle=angle, axis=axis.result))
+    expected_block.stmts.append(rot := op_stmts.Rot(angle=angle, axis=axis))
     expected_block.stmts.append(new_angle := py.USub(angle))
-    expected_block.stmts.append(new_axis := op_stmts.Adjoint(op=axis.result))
+    expected_block.stmts.append(new_axis := op_stmts.Adjoint(op=axis))
     expected_block.stmts.append(
         op_stmts.Rot(angle=new_angle.result, axis=new_axis.result)
     )
-
-    test_block.print()
-    expected_block.print()
     assert_nodes(test_block, expected_block)
