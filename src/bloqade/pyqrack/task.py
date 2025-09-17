66a71c9549c7b56c4fbf132a413831abd7cb1ecf
Jonathan Wurtz <jwurtz@quera.com>
2025-09-17 13:50:08 -0400

    add multi* methods to pyrack task

--- src/bloqade/pyqrack/task.py
+++ src/bloqade/pyqrack/task.py
@@ -7,6 +7,9 @@ from bloqade.pyqrack.base import (
     MemoryABC,
     PyQrackInterpreter,
 )
+from collections import Counter
+from kirin.dialects.ilist import IList
+import numpy as np
 
 RetType = TypeVar("RetType")
 Param = ParamSpec("Param")
@@ -51,3 +54,68 @@ class PyQrackSimulatorTask(AbstractSimulatorTask[Param, RetType, MemoryType]):
         except AttributeError:
             Warning("Task has not been run, there are no qubits!")
             return []
+
+    def multirun(self,shots:int) -> dict[RetType, float]:
+        """
+        Repeatedly run the task to collect statistics on the shot outcomes
+          The average is done over nshots and thus is frequentist and converges to
+          exact only in the shots -> infinity limit.
+        Parameters:
+        shots - the number of repetitions of the task
+        Returns:
+        dict[RetType, float] - a dictionary mapping outcomes to their probabilities,
+          as estimated from counting the shot outcomes. RetType must be hashable.
+        """
+        
+        results:list[RetType] = [self.run() for _ in range(shots)]
+        # Convert IList to tuple so that it is hashable by Counter
+        def convert(data):
+            if isinstance(data, (list,IList)):
+                return tuple(convert(item) for item in data)
+            return data
+        results = convert(results)
+        
+        data = {key: value/len(results) for key, value in Counter(results).items()} # Normalize to probabilities
+        return data
+    
+    def multistate(self,shots:int=1,selector:None=None) -> "np.linalg._linalg.EighResult":
+        """
+        Repeatedly run the task to extract the averaged quantum state.
+          The average is done over nshots and thus is frequentist and converges to
+          exact only in the shots -> infinity limit.
+        Parameters:
+        shots - the number of repetitions of the task
+        selector - an optional callable that takes the output of self.run() and extract
+          the [returned] qubits to be used for the quantum state. If None, all qubits
+          in the simulator are used. Other shan selector = None, the common other pattern is
+           > selector = lambda qubits: qubits
+          for the case where self.run() returns a list of qubits, or
+           > selector = lambda qubit: [qubits]
+          when the output is a single qubit.
+        Returns:
+        np.linalg._linalg.EighResult - the averaged quantum state as a density matrix,
+          represented in its eigenbasis.
+        """
+        # Import here to avoid circular dependencies.
+        from bloqade.pyqrack.device import PyQrackSimulatorBase
+        
+        states = []
+        for _ in range(shots):
+            res = self.run()
+            if callable(selector):
+                qbs = selector(res)
+            else:
+                qbs = self.qubits()
+            states.append(PyQrackSimulatorBase.quantum_state(qbs))
+            
+        state = np.linalg._linalg.EighResult(eigenvectors = np.concatenate([state.eigenvectors for state in states], axis=1),
+                                eigenvalues = np.concatenate([state.eigenvalues for state in states], axis=0)/len(states))
+        
+        # Canonicalize the state by orthoganalizing the basis vectors.
+        tol = 1e-7
+        s,v,d = np.linalg.svd(state.eigenvectors*np.sqrt(state.eigenvalues), full_matrices=False)
+        mask = v>tol
+        v = v[mask]**2
+        s = s[:,mask]
+        return np.linalg._linalg.EighResult(eigenvalues=v, eigenvectors=s)
+        
