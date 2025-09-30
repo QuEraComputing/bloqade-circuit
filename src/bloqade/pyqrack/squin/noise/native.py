c8bafccd1f2899de1056cd4468d5d02d273810bc
David Plankensteiner <da.plankensteiner@gmail.com>
2025-09-30 12:31:34 +0200

    Fix pyqrack methods

--- src/bloqade/pyqrack/squin/noise/native.py
+++ src/bloqade/pyqrack/squin/noise/native.py
@@ -1,12 +1,6 @@
-import random
-import typing
-from functools import cached_property
-from dataclasses import dataclass
-
 from kirin import interp
-from kirin.dialects import ilist
 
-from bloqade.pyqrack import QubitState, PyQrackQubit, PyQrackInterpreter
+from bloqade.pyqrack import PyQrackQubit, PyQrackInterpreter
 from bloqade.squin.noise.stmts import (
     QubitLoss,
     Depolarize,
@@ -16,70 +10,28 @@ from bloqade.squin.noise.stmts import (
 )
 from bloqade.squin.noise._dialect import dialect as squin_noise_dialect
 
-from ..runtime import KronRuntime, IdentityRuntime, OperatorRuntime, OperatorRuntimeABC
-
-
-@dataclass(frozen=True)
-class StochasticUnitaryChannelRuntime(OperatorRuntimeABC):
-    operators: (
-        ilist.IList[OperatorRuntimeABC, typing.Any] | tuple[OperatorRuntimeABC, ...]
-    )
-    probabilities: ilist.IList[float, typing.Any] | tuple[float, ...]
-
-    @property
-    def n_sites(self) -> int:
-        n = self.operators[0].n_sites
-        for op in self.operators[1:]:
-            assert (
-                op.n_sites == n
-            ), "Encountered a stochastic unitary channel with operators of different size!"
-        return n
-
-    def apply(self, *qubits: PyQrackQubit, adjoint: bool = False) -> None:
-        # NOTE: probabilities don't necessarily sum to 1; could be no noise event should occur
-        p_no_op = 1 - sum(self.probabilities)
-        if random.uniform(0.0, 1.0) < p_no_op:
-            return
-
-        selected_ops = random.choices(self.operators, weights=self.probabilities)
-        for op in selected_ops:
-            op.apply(*qubits, adjoint=adjoint)
-
-
-@dataclass(frozen=True)
-class QubitLossRuntime(OperatorRuntimeABC):
-    p: float
-
-    @property
-    def n_sites(self) -> int:
-        return 1
-
-    def apply(self, qubit: PyQrackQubit, adjoint: bool = False) -> None:
-        if random.uniform(0.0, 1.0) <= self.p:
-            qubit.state = QubitState.Lost
-
 
 @squin_noise_dialect.register(key="pyqrack")
 class PyQrackMethods(interp.MethodTable):
 
     single_pauli_choices = ("i", "x", "y", "z")
     two_pauli_choices = (
-        "II",
-        "IX",
-        "IY",
-        "IZ",
-        "XI",
-        "XX",
-        "XY",
-        "XZ",
-        "YI",
-        "YX",
-        "YY",
-        "YZ",
-        "ZI",
-        "ZX",
-        "ZY",
-        "ZZ",
+        "ii",
+        "ix",
+        "iy",
+        "iz",
+        "xi",
+        "xx",
+        "xy",
+        "xz",
+        "yi",
+        "yx",
+        "yy",
+        "yz",
+        "zi",
+        "zx",
+        "zy",
+        "zz",
     )
 
     @interp.impl(Depolarize)
@@ -87,7 +39,7 @@ class PyQrackMethods(interp.MethodTable):
         self, interp: PyQrackInterpreter, frame: interp.Frame, stmt: Depolarize
     ):
         p = frame.get(stmt.p)
-        ps = (p / 3.0,) * 3
+        ps = [p / 3.0] * 3
         qubits = frame.get(stmt.qubits)
         self.apply_single_qubit_pauli_error(interp, ps, qubits)
 
@@ -96,7 +48,7 @@ class PyQrackMethods(interp.MethodTable):
         self, interp: PyQrackInterpreter, frame: interp.Frame, stmt: Depolarize2
     ):
         p = frame.get(stmt.p)
-        ps = (p / 15.0,) * 15
+        ps = [p / 15.0] * 15
         controls = frame.get(stmt.controls)
         targets = frame.get(stmt.targets)
         self.apply_two_qubit_pauli_error(interp, ps, controls, targets)
@@ -112,7 +64,7 @@ class PyQrackMethods(interp.MethodTable):
         py = frame.get(stmt.py)
         pz = frame.get(stmt.pz)
         qubits = frame.get(stmt.qubits)
-        self.apply_single_qubit_pauli_error(interp, (px, py, pz), qubits)
+        self.apply_single_qubit_pauli_error(interp, [px, py, pz], qubits)
 
     @interp.impl(TwoQubitPauliChannel)
     def two_qubit_pauli_channel(
@@ -131,34 +83,19 @@ class PyQrackMethods(interp.MethodTable):
         self, interp: PyQrackInterpreter, frame: interp.Frame, stmt: QubitLoss
     ):
         p = frame.get(stmt.p)
-        return (QubitLossRuntime(p),)
-
-    @cached_property
-    def single_qubit_paulis(self):
-        return (OperatorRuntime("x"), OperatorRuntime("y"), OperatorRuntime("z"))
-
-    @cached_property
-    def two_qubit_paulis(self):
-        paulis = (IdentityRuntime(sites=1), *self.single_qubit_paulis)
-        ops: list[KronRuntime] = []
-        for idx1, pauli1 in enumerate(paulis):
-            for idx2, pauli2 in enumerate(paulis):
-                if idx1 == idx2 == 0:
-                    # NOTE: 'II'
-                    continue
-
-                ops.append(KronRuntime(pauli1, pauli2))
-
-        return tuple(ops)
+        qubits: list[PyQrackQubit] = frame.get(stmt.qubits)
+        for qbit in qubits:
+            if interp.rng_state.uniform(0.0, 1.0) <= p:
+                qbit.drop()
 
     def apply_single_qubit_pauli_error(
         self,
         interp: PyQrackInterpreter,
-        ps: tuple[float, float, float],
+        ps: list[float],
         qubits: list[PyQrackQubit],
     ):
         pi = 1 - sum(ps)
-        probs = (pi,) + ps
+        probs = [pi] + ps
 
         assert all(0 <= x <= 1 for x in probs), "Invalid Pauli error probabilities"
 
@@ -169,12 +106,12 @@ class PyQrackMethods(interp.MethodTable):
     def apply_two_qubit_pauli_error(
         self,
         interp: PyQrackInterpreter,
-        ps: tuple[float, ...],
+        ps: list[float],
         controls: list[PyQrackQubit],
         targets: list[PyQrackQubit],
     ):
         pii = 1 - sum(ps)
-        probs = (pii,) + ps
+        probs = [pii] + ps
         assert all(0 <= x <= 1 for x in probs), "Invalid Pauli error probabilities"
 
         for control, target in zip(controls, targets):
