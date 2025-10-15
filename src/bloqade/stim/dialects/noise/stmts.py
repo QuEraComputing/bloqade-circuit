d130835bcd563fc88e1bd2482a39a86e05650ad7
Rafael Haenel <rhaenel@quera.com>
2025-10-15 13:12:26 -0700

    Make NonStimCorrelatedError.nonce=0 instead of random value

--- src/bloqade/stim/dialects/noise/stmts.py
+++ src/bloqade/stim/dialects/noise/stmts.py
@@ -1,5 +1,3 @@
-import random
-
 from kirin import ir, types, lowering
 from kirin.decl import info, statement
 
@@ -91,8 +89,7 @@ class NonStimError(ir.Statement):
 class NonStimCorrelatedError(ir.Statement):
     name = "NonStimCorrelatedError"
     traits = frozenset({lowering.FromPythonCall()})
-    # nonce must be a unique value, otherwise stim might merge two correlated errors
-    nonce: int = info.attribute(default_factory=lambda: random.getrandbits(32))
+    nonce: int = info.attribute(default=0)
     probs: tuple[ir.SSAValue, ...] = info.argument(types.Float)
     targets: tuple[ir.SSAValue, ...] = info.argument(types.Int)
 
