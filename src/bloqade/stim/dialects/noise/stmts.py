03a335812e9f8973b9491217d3fcfc5ba3df8814
Rafael Haenel <rhaenel@quera.com>
2025-10-15 10:34:06 -0700

    Remove dynamic import

--- src/bloqade/stim/dialects/noise/stmts.py
+++ src/bloqade/stim/dialects/noise/stmts.py
@@ -1,3 +1,5 @@
+import random
+
 from kirin import ir, types, lowering
 from kirin.decl import info, statement
 
@@ -89,9 +91,8 @@ class NonStimError(ir.Statement):
 class NonStimCorrelatedError(ir.Statement):
     name = "NonStimCorrelatedError"
     traits = frozenset({lowering.FromPythonCall()})
-    nonce: int = info.attribute(
-        default_factory=lambda: __import__("random").getrandbits(32)
-    )  # Must be a unique value, otherwise stim might merge two correlated errors with equal probabilities
+    # nonce must be a unique value, otherwise stim might merge two correlated errors
+    nonce: int = info.attribute(default_factory=lambda: random.getrandbits(32))
     probs: tuple[ir.SSAValue, ...] = info.argument(types.Float)
     targets: tuple[ir.SSAValue, ...] = info.argument(types.Int)
 
