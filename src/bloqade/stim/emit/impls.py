507d49b2a9fca723e6c13cbdb476185920e88eea
kaihsin <khwu@quera.com>
2025-10-27 10:08:16 -0400

    add annotation codegen for stim

---
+++ src/bloqade/stim/emit/impls.py
@@ -0,0 +1,17 @@
+from kirin.emit import EmitStrFrame
+from kirin.interp import MethodTable, impl
+from kirin.dialects.debug import Info, dialect
+
+from bloqade.stim.emit.stim_str import EmitStimMain
+
+
+@dialect.register(key="emit.stim")
+class EmitStimDebugMethods(MethodTable):
+
+    @impl(Info)
+    def info(self, emit: EmitStimMain, frame: EmitStrFrame, stmt: Info):
+
+        msg: str = frame.get(stmt.msg)
+        emit.writeln(frame, f"#{msg}")
+
+        return ()
