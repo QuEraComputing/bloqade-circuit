78f5d2b842bd44d23108b188dc8acac0a00f84f9
Phillip Weinberg <weinbe58@gmail.com>
2025-10-28 14:17:58 -0400

    renaming variables in method table

--- src/bloqade/pyqrack/native.py
+++ src/bloqade/pyqrack/native.py
@@ -15,21 +15,21 @@ class NativeMethods(interp.MethodTable):
 
     @interp.impl(gate.CZ)
     def cz(self, _interp: PyQrackInterpreter, frame: interp.Frame, stmt: gate.CZ):
-        ctrls = frame.get_casted(stmt.controls, ilist.IList[PyQrackQubit, Any])
-        qargs = frame.get_casted(stmt.targets, ilist.IList[PyQrackQubit, Any])
+        controls = frame.get_casted(stmt.controls, ilist.IList[PyQrackQubit, Any])
+        targets = frame.get_casted(stmt.targets, ilist.IList[PyQrackQubit, Any])
 
-        for ctrl, qarg in zip(ctrls, qargs):
-            if ctrl.is_active() and qarg.is_active():
-                ctrl.sim_reg.mcz([ctrl.addr], qarg.addr)
+        for ctrl, trgt in zip(controls, targets):
+            if ctrl.is_active() and trgt.is_active():
+                ctrl.sim_reg.mcz([ctrl.addr], trgt.addr)
 
         return ()
 
     @interp.impl(gate.R)
     def r(self, _interp: PyQrackInterpreter, frame: interp.Frame, stmt: gate.R):
-        inputs = frame.get_casted(stmt.qubits, ilist.IList[PyQrackQubit, Any])
+        qubits = frame.get_casted(stmt.qubits, ilist.IList[PyQrackQubit, Any])
         rotation_angle = 2 * math.pi * frame.get_casted(stmt.rotation_angle, float)
         axis_angle = 2 * math.pi * frame.get_casted(stmt.axis_angle, float)
-        for qubit in inputs:
+        for qubit in qubits:
             if qubit.is_active():
                 qubit.sim_reg.r(Pauli.PauliZ, axis_angle, qubit.addr)
                 qubit.sim_reg.r(Pauli.PauliX, rotation_angle, qubit.addr)
@@ -39,10 +39,10 @@ class NativeMethods(interp.MethodTable):
 
     @interp.impl(gate.Rz)
     def rz(self, _interp: PyQrackInterpreter, frame: interp.Frame, stmt: gate.Rz):
-        inputs = frame.get_casted(stmt.qubits, ilist.IList[PyQrackQubit, Any])
+        qubits = frame.get_casted(stmt.qubits, ilist.IList[PyQrackQubit, Any])
         rotation_angle = 2 * math.pi * frame.get_casted(stmt.rotation_angle, float)
 
-        for qubit in inputs:
+        for qubit in qubits:
             if qubit.is_active():
                 qubit.sim_reg.r(Pauli.PauliZ, rotation_angle, qubit.addr)
 
