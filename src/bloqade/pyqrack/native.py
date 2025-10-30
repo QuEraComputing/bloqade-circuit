1ef747bb8fea406353df80fdee10243b589eaa3e
Phillip Weinberg <weinbe58@gmail.com>
2025-10-30 08:17:34 -0400

    removing export of statements

--- src/bloqade/pyqrack/native.py
+++ src/bloqade/pyqrack/native.py
@@ -7,14 +7,14 @@ from kirin.dialects import ilist
 from pyqrack import Pauli
 from bloqade.pyqrack import PyQrackQubit
 from bloqade.pyqrack.base import PyQrackInterpreter
-from bloqade.native.dialects import gate
+from bloqade.native.dialects.gate import stmts
 
 
-@gate.dialect.register(key="pyqrack")
+@stmts.dialect.register(key="pyqrack")
 class NativeMethods(interp.MethodTable):
 
-    @interp.impl(gate.CZ)
-    def cz(self, _interp: PyQrackInterpreter, frame: interp.Frame, stmt: gate.CZ):
+    @interp.impl(stmts.CZ)
+    def cz(self, _interp: PyQrackInterpreter, frame: interp.Frame, stmt: stmts.CZ):
         controls = frame.get_casted(stmt.controls, ilist.IList[PyQrackQubit, Any])
         targets = frame.get_casted(stmt.targets, ilist.IList[PyQrackQubit, Any])
 
@@ -24,8 +24,8 @@ class NativeMethods(interp.MethodTable):
 
         return ()
 
-    @interp.impl(gate.R)
-    def r(self, _interp: PyQrackInterpreter, frame: interp.Frame, stmt: gate.R):
+    @interp.impl(stmts.R)
+    def r(self, _interp: PyQrackInterpreter, frame: interp.Frame, stmt: stmts.R):
         qubits = frame.get_casted(stmt.qubits, ilist.IList[PyQrackQubit, Any])
         rotation_angle = 2 * math.pi * frame.get_casted(stmt.rotation_angle, float)
         axis_angle = 2 * math.pi * frame.get_casted(stmt.axis_angle, float)
@@ -37,8 +37,8 @@ class NativeMethods(interp.MethodTable):
 
         return ()
 
-    @interp.impl(gate.Rz)
-    def rz(self, _interp: PyQrackInterpreter, frame: interp.Frame, stmt: gate.Rz):
+    @interp.impl(stmts.Rz)
+    def rz(self, _interp: PyQrackInterpreter, frame: interp.Frame, stmt: stmts.Rz):
         qubits = frame.get_casted(stmt.qubits, ilist.IList[PyQrackQubit, Any])
         rotation_angle = 2 * math.pi * frame.get_casted(stmt.rotation_angle, float)
 
