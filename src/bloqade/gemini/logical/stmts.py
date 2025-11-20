724b0196324984cbb536fd4b824a3960582d6964
John Long <jlong@quera.com>
2025-11-20 10:17:25 -0500

    add docstring to original statement

--- src/bloqade/gemini/logical/stmts.py
+++ src/bloqade/gemini/logical/stmts.py
@@ -12,6 +12,19 @@ CodeN = types.TypeVar("CodeN", bound=types.Int)
 
 @statement(dialect=dialect)
 class TerminalLogicalMeasurement(ir.Statement):
+    """Perform measurements on a list of logical qubits.
+
+    Measurements are returned as a nested list where each member list
+    contains the individual measurement results for the constituent physical qubits per logical qubit.
+
+    Args:
+        qubits (IList[QubitType, Len]): The list of logical qubits
+
+    Returns:
+        IList[IList[MeasurementResultType, CodeN], Len]: A nested list containing the measurement results,
+            where each inner list corresponds to the measurements of the physical qubits that make up each logical qubit.
+    """
+
     traits = frozenset({lowering.FromPythonCall()})
     qubits: ir.SSAValue = info.argument(ilist.IListType[QubitType, Len])
     result: ir.ResultValue = info.result(
