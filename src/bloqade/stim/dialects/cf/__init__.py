6bbf11f92d61e2b4fdeae399f036e3ccd556cd10
John Long <jlong@quera.com>
2025-11-21 10:25:23 -0500

    add REPEAT statement in stim

---
+++ src/bloqade/stim/dialects/cf/__init__.py
@@ -0,0 +1,3 @@
+from .emit import EmitStimCfMethods as EmitStimCfMethods
+from .stmts import *  # noqa F403
+from ._dialect import dialect as dialect
---
+++ src/bloqade/stim/dialects/cf/stmts.py
@@ -0,0 +1,36 @@
+from typing import cast
+
+from kirin import ir, types
+from kirin.decl import info, statement
+
+from ._dialect import dialect
+
+
+@statement(dialect=dialect, init=False)
+class REPEAT(ir.Statement):
+    """Repeat statement for looping a fixed number of times.
+
+    This statement has a loop count and a body.
+    """
+
+    name = "REPEAT"
+    traits = frozenset({ir.MaybePure(), ir.HasCFG(), ir.SSACFG()})
+    count: ir.SSAValue = info.argument(types.Int)
+    body: ir.Region = info.region(multi=False)
+
+    def __init__(
+        self,
+        count: ir.SSAValue,
+        body: ir.Region | ir.Block,
+    ):
+        if body.IS_REGION:
+            body_region = cast(ir.Region, body)
+            if body_region.blocks:
+                body_block = body_region.blocks[0]
+            else:
+                body_block = None
+        else:
+            body_block = cast(ir.Block, body)
+            body_region = ir.Region(body_block)
+
+        super().__init__(args=(count,), regions=(body_region,), args_slice={"count": 0})
