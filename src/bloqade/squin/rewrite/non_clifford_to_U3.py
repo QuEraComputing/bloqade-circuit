22aee6e83650fbb0da6f740291832143414d62d5
Phillip Weinberg <weinbe58@gmail.com>
2025-12-04 15:54:49 -0500

    fixing bugs + adding tests

--- src/bloqade/squin/rewrite/non_clifford_to_U3.py
+++ src/bloqade/squin/rewrite/non_clifford_to_U3.py
@@ -15,9 +15,22 @@ class RewriteNonCliffordToU3(rewrite_abc.RewriteRule):
     - Ry gate to U3 gate with parameters (angle, 0, 0)
     - Rz gate is U3 gate with parameters (0, 0, angle)
 
+    This rewrite should be paired with `U3ToClifford` to canonicalize the circuit.
+
     """
 
     def rewrite_Statement(self, node: ir.Statement) -> rewrite_abc.RewriteResult:
+        if not isinstance(
+            node,
+            (
+                gate_stmts.T,
+                gate_stmts.Rx,
+                gate_stmts.Ry,
+                gate_stmts.Rz,
+            ),
+        ):
+            return rewrite_abc.RewriteResult()
+
         rule = getattr(self, f"rewrite_{type(node).__name__}", self.default)
 
         return rule(node)
@@ -71,6 +84,21 @@ class RewriteNonCliffordToU3(rewrite_abc.RewriteRule):
                 theta=node.angle,
                 phi=phi_stmt.result,
                 lam=lam_stmt.result,
+            )
+        )
+
+        return rewrite_abc.RewriteResult(has_done_something=True)
+
+    def rewrite_Rz(self, node: gate_stmts.Rz) -> rewrite_abc.RewriteResult:
+        (theta_stmt := py.Constant(0.0)).insert_before(node)
+        (phi_stmt := py.Constant(0.0)).insert_before(node)
+
+        node.replace_by(
+            gate_stmts.U3(
+                qubits=node.qubits,
+                theta=theta_stmt.result,
+                phi=phi_stmt.result,
+                lam=node.angle,
             )
         )
 
