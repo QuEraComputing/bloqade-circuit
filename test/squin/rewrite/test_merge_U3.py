8b3eddbe96b05f33ad8154f269ece256e9df89bf
Ludwig Schmid <lschmid@quera.com>
2026-01-28 08:37:58 +0100

    removed non-valid test case

--- test/squin/rewrite/test_merge_U3.py
+++ test/squin/rewrite/test_merge_U3.py
@@ -1,5 +1,3 @@
-import math
-
 import numpy as np
 from kirin import ir, rewrite
 from kirin.dialects import py
@@ -209,62 +207,6 @@ def test_rewrite_nonconstant_param_no_merge():
     )
 
     rule = rewrite.Walk(RewriteMergeU3())
-    rule.rewrite(test_block)
-
-    assert_nodes(test_block, expected_block)
-
-
-def test_rewrite_three_u3_merges_to_one_equivalent_matrix():
-    test_qubits = ir.TestValue()
-    # random-ish parameters in turns (same convention as RewriteMergeU3)
-    # Keep theta away from 0 to avoid ambiguous decompositions.
-
-    theta1, phi1, lam1 = 0.3, 0.1, -0.2
-    theta2, phi2, lam2 = -0.7, 0.9, 0.4
-    theta3, phi3, lam3 = math.pi / 3, -math.pi / 5, math.pi / 7
-
-    c_t1, c_p1, c_l1 = py.Constant(theta1), py.Constant(phi1), py.Constant(lam1)
-    c_t2, c_p2, c_l2 = py.Constant(theta2), py.Constant(phi2), py.Constant(lam2)
-    c_t3, c_p3, c_l3 = py.Constant(theta3), py.Constant(phi3), py.Constant(lam3)
-
-    test_block = ir.Block(
-        [
-            gate_stmts.U3(
-                qubits=test_qubits,
-                theta=c_t1.result,
-                phi=c_p1.result,
-                lam=c_l1.result,
-            ),
-            gate_stmts.U3(
-                qubits=test_qubits,
-                theta=c_t2.result,
-                phi=c_p2.result,
-                lam=c_l2.result,
-            ),
-            gate_stmts.U3(
-                qubits=test_qubits,
-                theta=c_t3.result,
-                phi=c_p3.result,
-                lam=c_l3.result,
-            ),
-        ]
-    )
-    expected_theta = py.Constant(0.8657155563999239)
-    expected_phi = py.Constant(-1.144168331300085)
-    expected_lam = py.Constant(1.901389486255007)
-    expected_block = ir.Block(
-        [
-            gate_stmts.U3(
-                qubits=test_qubits,
-                theta=expected_theta.result,
-                phi=expected_phi.result,
-                lam=expected_lam.result,
-            ),
-        ]
-    )
-
-    rule = rewrite.Walk(RewriteMergeU3())
-    rule.rewrite(test_block)
     rule.rewrite(test_block)
 
     assert_nodes(test_block, expected_block)
