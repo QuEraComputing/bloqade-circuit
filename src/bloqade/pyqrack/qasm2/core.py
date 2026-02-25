22faad9ab480cbc22a033c6548365349ca98890f
David Plankensteiner <dplankensteiner@quera.com>
2026-02-25 12:28:41 +0100

    Fix QRegEq in pyqrack

--- src/bloqade/pyqrack/qasm2/core.py
+++ src/bloqade/pyqrack/qasm2/core.py
@@ -90,9 +90,40 @@ class PyQrackMethods(interp.MethodTable):
     def creg_eq(
         self, interp: PyQrackInterpreter, frame: interp.Frame, stmt: core.CRegEq
     ):
-        lhs: CRegister = frame.get(stmt.lhs)
-        rhs: CRegister = frame.get(stmt.rhs)
-        if len(lhs) != len(rhs):
-            return (False,)
+        lhs: CRegister | CBitRef | int = frame.get(stmt.lhs)
+        rhs: CRegister | CBitRef | int = frame.get(stmt.rhs)
 
-        return (all(left is right for left, right in zip(lhs, rhs)),)
+        if isinstance(lhs, CBitRef):
+            lhs = bool(lhs.get_value())
+        if isinstance(rhs, CBitRef):
+            rhs = bool(rhs.get_value())
+
+        def check_bit(lhs: bool, rhs: int, pos: int):
+            # compares the bit lhs to the corresponding bit
+            # at pos in the rhs integer; uses little-endian ordering
+            # which is how QASM2 does it
+            return lhs == ((rhs << pos) & 1)
+
+        match (lhs, rhs):
+            case (CRegister(), CRegister()):
+                if len(lhs) != len(rhs):
+                    return (False,)
+
+                return (all(left is right for left, right in zip(lhs, rhs)),)
+
+            case (CRegister(), int()):
+                for i, bit in enumerate(lhs):
+                    if not check_bit(bool(bit), rhs, i):
+                        return (False,)
+
+                return (True,)
+
+            case (int(), CRegister()):
+                for i, bit in enumerate(rhs):
+                    if not check_bit(bool(bit), lhs, i):
+                        return (False,)
+
+                return (True,)
+
+            case (int(), int()):
+                return (lhs == rhs,)
---
+++ test/qasm2/test_ifs.py
@@ -0,0 +1,86 @@
+from bloqade import qasm2
+from bloqade.pyqrack import StackMemorySimulator
+from bloqade.qasm2.emit import QASM2
+
+
+def test_simulator():
+
+    @qasm2.main
+    def main():
+        qreg = qasm2.qreg(2)
+        creg = qasm2.creg(2)
+        qasm2.x(qreg[0])
+
+        # NOTE: all these should evaluate to True
+        if creg[0] == 1:
+            qasm2.x(qreg[1])
+
+        qasm2.measure(qreg, creg)
+        if creg == 3:
+            qasm2.x(qreg[1])
+
+        qasm2.measure(qreg, creg)
+        if 1 == creg:
+            qasm2.x(qreg[1])
+
+        qasm2.measure(qreg, creg)
+        if creg == creg:
+            qasm2.x(qreg[1])
+
+        # NOTE: we skipped a measurement so we should be in 11 still
+        if creg[0] == creg[1]:
+            qasm2.x(qreg[1])
+
+        qasm2.measure(qreg, creg)
+        return creg
+
+    sim = StackMemorySimulator(min_qubits=2)
+    task = sim.task(main)
+    result = task.run()
+    assert result == [1, 1]
+
+
+def test_emit():
+    @qasm2.main
+    def main():
+        qreg = qasm2.qreg(2)
+        creg = qasm2.creg(2)
+        qasm2.x(qreg[0])
+
+        # NOTE: all these should evaluate to True
+        if creg[0] == 1:
+            qasm2.x(qreg[1])
+
+        qasm2.measure(qreg, creg)
+        if creg == 3:
+            qasm2.x(qreg[1])
+
+        qasm2.measure(qreg, creg)
+        if 1 == creg:
+            qasm2.x(qreg[1])
+
+        qasm2.measure(qreg, creg)
+        if creg == creg:
+            qasm2.x(qreg[1])
+
+        # NOTE: we skipped a measurement so we should be in 11 still
+        if creg[0] == creg[1]:
+            qasm2.x(qreg[1])
+
+        qasm2.measure(qreg, creg)
+
+        # NOTE: this should be False
+        if creg == 0:
+            qasm2.x(qreg[1])
+
+        qasm2.measure(qreg, creg)
+        if creg == 1:
+            qasm2.x(qreg[1])
+
+        qasm2.measure(qreg, creg)
+        return creg
+
+    target = QASM2()
+    # qasm2_str = target.emit_str(main)
+
+    qasm2.parse.pprint(target.emit(main))
