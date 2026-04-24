Fix factor of 2 in phased xz (#752)
Documentation was misleading. In all cases the args were treated as
turns with the exception of the stdlib implementation in the native
dialect.

Separately, the `shift` gate was missing a factor of 2 in the input
phase.

Added integration tests based on pyqrack.

test/native/test_stdlib.py
@@ -183,10 +183,8 @@ def test_phased_xz_against_squin(x_rad: float, z_rad: float, axis_phase_rad: flo
     sv_native = np.asarray(sv_native)
     sv_squin = np.asarray(sv_squin)
 
-    native_i = np.abs(sv_native).argmax()
-    sv_native *= np.exp(-1j * np.angle(sv_native[native_i]))
-
-    squin_i = np.abs(sv_squin).argmax()
-    sv_squin *= np.exp(-1j * np.angle(sv_squin[squin_i]))
+    i = np.abs(sv_native).argmax()
+    sv_native *= np.exp(-1j * np.angle(sv_native[i]))
+    sv_squin *= np.exp(-1j * np.angle(sv_squin[i]))
 
     assert np.allclose(sv_native, sv_squin, atol=1e-6)