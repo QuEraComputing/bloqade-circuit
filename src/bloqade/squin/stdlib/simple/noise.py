f6525d836dd989229ec3e9dccb5a79c5792b2633
John Long <jlong@quera.com>
2026-07-27 11:43:45 -0400

    Incorporate Kai's feedback, increase coverage + docs too

--- src/bloqade/squin/stdlib/simple/noise.py
+++ src/bloqade/squin/stdlib/simple/noise.py
@@ -112,7 +112,17 @@ def correlated_qubit_loss(p: float, qubits: ilist.IList[Qubit, Any]) -> None:
 
 
 @kernel
-def qubit_leakage(p0: float, p1: float, qubit) -> None:
+def qubit_leakage(p0: float, p1: float, qubit: Qubit) -> None:
+    """
+    Apply a leakage channel to the given qubit.
+
+    The qubit leaks to 0 with a probability `p0` and to 1 with a probability `p1`.
+
+    Args:
+        p0 (float): Probability of the qubit leaking to 0.
+        p1 (float): Probability of the qubit leaking to 1.
+        qubit (Qubit): The qubit to which the noise channel is applied.
+    """
     broadcast.qubit_leakage(p0, p1, ilist.IList([qubit]))
 
 
